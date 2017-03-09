1b465f056c4a792dbea9e26a96e7ab8528e432ef
Liu Gang <Gang.Liu@nxp.com>
2017-03-09 15:59:17 +0800

    package:fmucode/rcw: Fix the fmucode and rcw package makefile
    Fix the fmucode and rcw package makefile action.
    If there is no any action, the buildroot will not download
    the package, so just give a echo action here.
    
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>

--- package/fmucode/fmucode.mk
+++ package/fmucode/fmucode.mk
@@ -9,7 +9,8 @@ FMUCODE_SITE = git://git.freescale.com/ppc/sdk/fm-ucode.git
 FMUCODE_LICENSE = Freescale-Binary-EULA
 FMUCODE_LICENSE_FILES = Freescale-Binary-EULA
 
-FMUCODE_INSTALL_TARGET = NO
-FMUCODE_BUILD_CMDS = NO
+define FMUCODE_CONFIGURE_CMDS
+       echo "No building is needed for fmucode"
+endef
 
 $(eval $(generic-package))
--- package/rcw/rcw.mk
+++ package/rcw/rcw.mk
@@ -9,7 +9,8 @@ RCW_SITE = git://git.freescale.com/ppc/sdk/ls2-rcw.git
 RCW_LICENSE = Freescale-Binary-EULA
 RCW_LICENSE_FILES = Freescale-Binary-EULA
 
-RCW_INSTALL_TARGET = NO
-RCW_BUILD_CMDS = NO
+define RCW_CONFIGURE_CMDS
+       echo "No building is needed for rcw"
+endef
 
 $(eval $(generic-package))
