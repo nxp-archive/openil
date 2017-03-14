d171fdf52f563559ff47ad812cf712d018f80a43
Po Liu <Po.Liu@nxp.com>
2017-03-14 14:23:46 +0800

    pyang: add support pyang config
    Signed-off-by: Po Liu <Po.Liu@nxp.com>

---
+++ package/pyang/pyang.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# PYANG
+#
+################################################################################
+
+PYANG_VERSION = master
+PYANG_SITE = https://github.com/mbj4668/pyang.git 
+PYANG_SITE_METHOD = git
+PYANG_INSTALL_STAGING = YES
+PYANG_LICENSE = MIT
+PYANG_LICENSE_FILES = COPYING
+PYANG_SETUP_TYPE = setuptools
+PYANG_DEPENDENCIES = libxml2
+
+define PYANG_INSTALL_STAGING_CMDS
+	export PYTHONPATH=$(HOST_DIR)/usr/lib/python2.7/site-packages
+	cd $(@D);$(HOST_DIR)/usr/bin/python $(@D)/setup.py install --prefix=$(HOST_DIR)/usr
+endef
+
+$(eval $(python-package))
+$(eval $(host-python-package))
+
