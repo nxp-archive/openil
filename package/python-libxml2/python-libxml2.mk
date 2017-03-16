cdd9f6c450e576ae085c3d9a3760894b8a883d81
Po Liu <Po.Liu@nxp.com>
2017-03-16 14:12:06 +0800

    python-libxml2: bugfix for netopeer compile issue
    Signed-off-by: Po Liu <Po.Liu@nxp.com>

--- package/python-libxml2/python-libxml2.mk
+++ package/python-libxml2/python-libxml2.mk
@@ -10,7 +10,6 @@ PYTHON_LIBXML2_LICENSE = MIT
 PYTHON_LIBXML2_LICENSE_FILES = COPYING
 PYTHON_LIBXML2_SETUP_TYPE = distutils
 PYTHON_LIBXML2_DEPENDENCIES = libxml2
-
 define PYTHON_LIBXML2_CONFIGURE_CMDS
 	$(APPLY_PATCHES) $(@D) package/python-libxml2 0001-python-libxml2-Add-python-libxml2-support-on-target-.patch
 	echo "python_libxml2_lib_dir = $(STAGING_DIR)/usr/lib" >> $(@D)/Setup
@@ -21,6 +20,7 @@ define PYTHON_LIBXML2_POST_INSTALL_TARGET_FIXUP
 	$(INSTALL) -D -m 0755 $(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages/libxml2.py* $(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/
 	$(INSTALL) -D -m 0755 $(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages/libxml2mod.so $(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/ 
 	$(INSTALL) -D -m 0755 $(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages/drv_libxml2.py* $(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/
+    	rm $(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages/*libxml2*
 endef
 
 PYTHON_LIBXML2_POST_INSTALL_TARGET_HOOKS +=PYTHON_LIBXML2_POST_INSTALL_TARGET_FIXUP
