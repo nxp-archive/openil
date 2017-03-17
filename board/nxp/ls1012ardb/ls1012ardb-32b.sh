965f0c1b2447e5c94679e895a167965638ddc585
Liu Gang <Gang.Liu@nxp.com>
2017-03-17 11:31:33 +0800

    board:nxp:ls1012ardb: Add ppa support
    Build the ppa.itb firmware.
    
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>

--- board/nxp/ls1012ardb/ls1012ardb-32b.sh
+++ board/nxp/ls1012ardb/ls1012ardb-32b.sh
@@ -4,6 +4,7 @@
 # Change the toolchain from aarch64 to armv7 to build 32b kernel and fs
 #
 # ${2} - uboot building directory
+# ${3} - top directory
 
 echo ${2}
 echo ${3}
@@ -16,7 +17,13 @@ mkdir board/nxp/ls1012ardb/temp
 cp output/images/* board/nxp/ls1012ardb/temp/
 cp ${2}/tools/mkimage board/nxp/ls1012ardb/temp/
 
+# Copy the uboot mkimage to output/host/usr/bin for the PPA building
+cp ${2}/tools/mkimage output/host/usr/bin
+make ppa-build
+cp ${BUILD_DIR}/ppa-fsl-sdk-v2.0-1701/ppa/soc-ls1012/build/obj/ppa.itb board/nxp/ls1012ardb/temp/
+
 make clean
+rm .config
 
 cp board/nxp/ls1012ardb/ls1012ardb-32b_linux_fs_defconfig configs/ls1012ardb-32b_linux_fs_defconfig
 
