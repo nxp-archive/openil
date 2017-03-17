66a403ae2a7e9ab6ebd482132dfe75d93709d626
Liu Gang <Gang.Liu@nxp.com>
2017-03-17 11:05:41 +0800

    package:ppa: Add ppa support for layerscape platforms
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>

---
+++ package/ppa/ppa.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# ppa firmware for NXP layerscape platforms
+#
+################################################################################
+
+PPA_VERSION = fsl-sdk-v2.0-1701
+PPA_SITE = git://git.freescale.com/ppc/sdk/ppa-generic.git
+PPA_LICENSE = Freescale-EULA
+PPA_LICENSE_FILES = EULA
+
+# export ARMV8_TOOLS_DIR="$(dir ${TARGET_CROSS})";\
+# export ARMV8_TOOLS_PREFIX="$(notdir ${TARGET_CROSS})";\
+#
+
+define PPA_BUILD_CMDS
+	export PATH=${PATH}:$(dir ${TARGET_CROSS});\
+	export CROSS_COMPILE="${TARGET_CROSS}";\
+	cd $(@D)/ppa/ && ./build rdb-fit all
+endef
+
+$(eval $(generic-package))
