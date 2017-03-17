e3d4a3f1052dd5d1e5aef9e37a2263e97a8a0405
Liu Gang <Gang.Liu@nxp.com>
2017-03-17 11:06:31 +0800

    board:nxp:ls1043ardb: Add ppa support
    Build the ppa.itb firmware and program into the SD image.
    
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>

--- board/nxp/ls1043ardb/ls1043ardb-32b.sh
+++ board/nxp/ls1043ardb/ls1043ardb-32b.sh
@@ -4,11 +4,10 @@
 # Change the toolchain from aarch64 to armv7 to build 32b kernel and fs
 #
 # ${2} - uboot building directory
+# ${3} - top directory
 
 echo ${2}
 echo ${3}
-
-rm .config
 
 rm -rf board/nxp/ls1043ardb/temp
 mkdir board/nxp/ls1043ardb/temp
@@ -16,7 +15,13 @@ mkdir board/nxp/ls1043ardb/temp
 cp output/images/* board/nxp/ls1043ardb/temp/
 cp ${2}/tools/mkimage board/nxp/ls1043ardb/temp/
 
+# Copy the uboot mkimage to output/host/usr/bin for the PPA building
+cp ${2}/tools/mkimage output/host/usr/bin
+make ppa-build
+cp ${BUILD_DIR}/ppa-fsl-sdk-v2.0-1701/ppa/soc-ls1043/build/obj/ppa.itb board/nxp/ls1043ardb/temp/
+
 make clean
+rm .config
 
 cp board/nxp/ls1043ardb/ls1043ardb-32b_linux_fs_defconfig configs/ls1043ardb-32b_linux_fs_defconfig
 
