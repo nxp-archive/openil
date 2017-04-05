c1ce6ab210d4d0fc711b016c3e706a9c351792a0
Po Liu <po.liu@nxp.com>
2017-04-05 16:03:20 +0800

    yang-cfginterfaces: cfginterfaces-init auto start and netopeer-server auto start
    Signed-off-by: Po Liu <po.liu@nxp.com>

--- package/yang-cfginterfaces/yang-cfginterfaces.mk
+++ package/yang-cfginterfaces/yang-cfginterfaces.mk
@@ -16,11 +16,20 @@ YANG_CFGINTERFACES_CONF_ENV += PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig
 YANG_CFGINTERFACES_CONF_ENV += PYTHON_CONFIG="$(STAGING_DIR)/usr/bin/python-config"
 YANG_CFGINTERFACES_CONF_ENV += ac_cv_path_NETOPEER_MANAGER="$(HOST_DIR)/usr/bin/netopeer-manager.host"
 YANG_CFGINTERFACES_CONF_ENV += ac_cv_file__etc_debian_version=yes
-#define YANG_CFGINTERFACES_INSTALL_INITCMD
-#	$(INSTALL) -D -m 0755 $(@D)/cfginterfaces-init $(TARGET_DIR)/usr/bin/
-#endef
 
-#YANG_CFGINTERFACES_POST_INSTALL_TARGET_HOOKS +=YANG_CFGINTERFACES_INSTALL_INITCMD
+define YANG_CFGINTERFACES_UPDATE_INIT_BIN
+	sed -i "s/.\/model/\/usr\/local\/etc\/netopeer\/ietf-interfaces/g"  `grep "./model" -rl $(@D)/cfginterfaces-init.c`
+endef
+
+YANG_CFGINTERFACES_POST_CONFIGURE_HOOKS += YANG_CFGINTERFACES_UPDATE_INIT_BIN 
+
+define YANG_CFGINTERFACES_INSTALL_INITCMD
+	$(INSTALL) -D -m 0755 $(@D)/cfginterfaces-init $(TARGET_DIR)/usr/local/bin/; \
+	$(INSTALL) -D -m 0755 $(@D)/model/ietf-yang-types.yin $(TARGET_DIR)/usr/local/etc/netopeer/ietf-interfaces/; \
+	$(INSTALL) -D -m 0755 $(@D)/model/ietf-inet-types.yin $(TARGET_DIR)/usr/local/etc/netopeer/ietf-interfaces/
+endef
+
+YANG_CFGINTERFACES_POST_INSTALL_TARGET_HOOKS +=YANG_CFGINTERFACES_INSTALL_INITCMD
 
 define YANG_CFGINTERFACES_CONFIGURE_CMDS
 	cd $(@D);$(TARGET_CONFIGURE_ARGS) $(TARGET_CONFIGURE_OPTS) $(YANG_CFGINTERFACES_CONF_ENV) \
