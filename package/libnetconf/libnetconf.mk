8dcbec2f8d7acc8500651659733538468473c6e8
Po Liu <po.liu@nxp.com>
2017-04-06 20:03:21 +0800

    lnctool: use buildroot host python and libxml2 lib
    Signed-off-by: Po Liu <po.liu@nxp.com>

--- package/libnetconf/libnetconf.mk
+++ package/libnetconf/libnetconf.mk
@@ -12,13 +12,15 @@ LIBNETCONF_LICENSE = MIT
 LIBNETCONF_LICENSE_FILES = COPYING
 LIBNETCONF_CONF_OPTS += --with-rpm
 HOST_LIBNETCONF_DEPENDENCIES = host-pkgconf pyang
-LIBNETCONF_DEPENDENCIES = libxml2 openssl libgcrypt libssh libxslt ncurses readline dbus pyang python-libxml2 host-python
+LIBNETCONF_DEPENDENCIES = libxml2 openssl libgcrypt libssh libxslt ncurses readline dbus pyang python-libxml2 host-python host-h-python-libxml2
 LIBNETCONF_CONF_ENV += PKG_CONFIG=$(HOST_DIR)/usr/bin/pkg-config
 LIBNETCONF_CONF_ENV +=PATH=$(STAGING_DIR)/usr/bin:$(BR_PATH)
 
+LIBNETCONF_POST_INSTALL_HOOKS += LIBNETCONF_UPDATE_LNCTOOL 
+
 define LIBNETCONF_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/dev-tools/lnctool/lnctool $(HOST_DIR)/usr/bin
-	$(INSTALL) -D -m 0755 $(@D)/libnetconf.pc $(STAGING_DIR)/usr/lib/pkgconfig
+	sed -i '1c\#!$(HOST_DIR)/usr/bin/python'  $(HOST_DIR)/usr/bin/lnctool
 	$(INSTALL) -D -m 0755 $(@D)/.libs/libnetconf.so* $(STAGING_DIR)/usr/lib/
 	$(INSTALL) -D -m 0755 $(@D)/headers/*.h $(STAGING_DIR)/usr/include
 	mkdir -p $(STAGING_DIR)/usr/include/libnetconf
@@ -28,5 +30,5 @@ define LIBNETCONF_INSTALL_STAGING_CMDS
 endef
 
 $(eval $(autotools-package))
-$(eval $(host-autotools-package))
+#$(eval $(host-autotools-package))
 
