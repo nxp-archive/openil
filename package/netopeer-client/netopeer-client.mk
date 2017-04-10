30f7eb6b118ac2e1ec9119b7e1a3a84f0e8f1861
Po Liu <po.liu@nxp.com>
2017-04-10 14:33:14 +0800

    netopeer-client: Add basic netopeer-client
    User could use it as localhost to test netopeer-server.
    
    Signed-off-by: Po Liu <po.liu@nxp.com>

---
+++ package/netopeer-client/netopeer-client.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# netopeer-cli
+#
+################################################################################
+NETOPEER_CLIENT_VERSION = $(NETOPEER_VERSION)
+NETOPEER_CLIENT_SITE = $(BUILD_DIR)/netopeer-$(NETOPEER_VERSION)/cli
+NETOPEER_CLIENT_SITE_METHOD = local
+NETOPEER_CLIENT_LICENSE = MIT
+NETOPEER_CLIENT_LICENSE_FILES = COPYING
+NETOPEER_CLIENT_DEPENDENCIES = netopeer
+NETOPEER_CLIENT_CONF_OPTS += --prefix=/usr/local/
+NETOPEER_CLIENT_CONF_OPTS += --with-libxml2=$(STAGING_DIR)/usr/bin
+$(eval $(autotools-package))
