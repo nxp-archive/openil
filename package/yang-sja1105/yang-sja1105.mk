15e0347b860df1dace4d94222aad055340fab8fa
Po Liu <po.liu@nxp.com>
2017-04-11 17:26:29 +0800

    yang-sja1105/yang-cfginterfaces: fix potential libxml2 lib not found issue
    Signed-off-by: Po Liu <po.liu@nxp.com>

--- package/yang-sja1105/yang-sja1105.mk
+++ package/yang-sja1105/yang-sja1105.mk
@@ -9,9 +9,6 @@ YANG_SJA1105_SITE_METHOD = local
 YANG_SJA1105_LICENSE = MIT
 YANG_SJA1105_LICENSE_FILES = COPYING
 YANG_SJA1105_DEPENDENCIES = libxml2 pyang libnetconf sja1105-tool
-YANG_SJA1105_CONF_ENV += CFLAGS="-I$(STAGING_DIR)/usr/include/libxml2 -pthread"
-YANG_SJA1105_CONF_ENV += LIBS="-L$(STAGING_DIR)/usr/lib"
-YANG_SJA1105_CONF_ENV += XML2_CONFIG=$(STAGING_DIR)/usr/bin/xml2-config
 YANG_SJA1105_CONF_ENV += PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig"
 YANG_SJA1105_CONF_ENV += PYTHON_CONFIG="$(STAGING_DIR)/usr/bin/python-config"
 YANG_SJA1105_CONF_ENV += ac_cv_path_NETOPEER_MANAGER="$(HOST_DIR)/usr/bin/netopeer-manager.host"
@@ -31,7 +28,8 @@ define YANG_SJA1105_CONFIGURE_CMDS
 	$(TARGET_CONFIGURE_ARGS) $(TARGET_CONFIGURE_OPTS) $(YANG_SJA1105_CONF_ENV) \
 	    ./configure  --prefix=/usr/local/ \
 	    --host=arm-buildroot-linux-gnueabihf \
-	    --build=x86_64-pc-linux-gnu
+	    --build=x86_64-pc-linux-gnu \
+	    --with-libxml2=$(STAGING_DIR)/usr/bin
 endef
 
 define YANG_SJA1105_LNCTOOL_CREATE_FILES
