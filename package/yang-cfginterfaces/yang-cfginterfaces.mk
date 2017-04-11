8098ab93bf728e1b93ea8d94bfdda7f0287a02a6
Po Liu <po.liu@nxp.com>
2017-04-11 17:26:29 +0800

    yang-sja1105/yang-cfginterfaces: fix potential libxml2 lib not found issue
    Signed-off-by: Po Liu <po.liu@nxp.com>

--- package/yang-cfginterfaces/yang-cfginterfaces.mk
+++ package/yang-cfginterfaces/yang-cfginterfaces.mk
@@ -9,13 +9,11 @@ YANG_CFGINTERFACES_SITE_METHOD = local
 YANG_CFGINTERFACES_LICENSE = MIT
 YANG_CFGINTERFACES_LICENSE_FILES = COPYING
 YANG_CFGINTERFACES_DEPENDENCIES = libxml2 pyang libnetconf
-YANG_CFGINTERFACES_CONF_ENV += CFLAGS="-I$(STAGING_DIR)/usr/include/libxml2 -pthread"
-YANG_CFGINTERFACES_CONF_ENV += LIBS="-L$(STAGING_DIR)/usr/lib"
-YANG_CFGINTERFACES_CONF_ENV += XML2_CONFIG=$(STAGING_DIR)/usr/bin/xml2-config
 YANG_CFGINTERFACES_CONF_ENV += PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig"
 YANG_CFGINTERFACES_CONF_ENV += PYTHON_CONFIG="$(STAGING_DIR)/usr/bin/python-config"
 YANG_CFGINTERFACES_CONF_ENV += ac_cv_path_NETOPEER_MANAGER="$(HOST_DIR)/usr/bin/netopeer-manager.host"
 YANG_CFGINTERFACES_CONF_ENV += ac_cv_file__etc_debian_version=yes
+YANG_CFGINTERFACES_CONF_ENV += WITH_LIBXML2_DIR=$(STAGING_DIR)/usr/bin/ 
 
 define YANG_CFGINTERFACES_UPDATE_INIT_BIN
 	sed -i "s/.\/model/\/usr\/local\/etc\/netopeer\/ietf-interfaces/g"  `grep "./model" -rl $(@D)/cfginterfaces-init.c`
@@ -35,7 +33,7 @@ define YANG_CFGINTERFACES_CONFIGURE_CMDS
 	cd $(@D);$(TARGET_CONFIGURE_ARGS) $(TARGET_CONFIGURE_OPTS) $(YANG_CFGINTERFACES_CONF_ENV) \
 	    ./configure  --prefix=/usr/local/ \
 	    --host=arm-buildroot-linux-gnueabihf \
-	    --build=x86_64-pc-linux-gnu 
+	    --build=x86_64-pc-linux-gnu
 endef
 
 $(eval $(autotools-package))
