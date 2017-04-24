da54beee67e853cf887caaa30b3b7252a7bd762f
Po Liu <po.liu@nxp.com>
2017-04-24 18:02:30 +0800

    sja1105-tool: change download server to github
    Use tag v0.2-rc1.
    
    Signed-off-by: Po Liu <po.liu@nxp.com>

--- package/sja1105-tool/sja1105-tool.mk
+++ package/sja1105-tool/sja1105-tool.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-SJA1105_TOOL_VERSION = devel
-SJA1105_TOOL_SITE =ssh://git@sw-stash.freescale.net/dnind/sja1105-tool.git 
+SJA1105_TOOL_VERSION = v0.2-rc1
+SJA1105_TOOL_SITE = https://github.com/openil/sja1105-tool.git
 SJA1105_TOOL_SITE_METHOD = git
 SJA1105_TOOL_INSTALL_STAGING = YES
 SJA1105_TOOL_LICENSE = BSD-3c
@@ -13,7 +13,8 @@ SJA1105_TOOL_LICENSE_FILES = COPYING
 SJA1105_TOOL_DEPENDENCIES = libxml2
 
 define SJA1105_TOOL_BUILD_CMDS
-	cd $(@D); $(TARGET_CONFIGURE_ARGS) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(TARGET_MAKE_OPTS) -C $(@D);
+	cd $(@D); $(SED) '/\<VERSION /c\VERSION  = $(SJA1105_TOOL_VERSION)' $(@D)/Makefile; \
+	$(TARGET_CONFIGURE_ARGS) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(TARGET_MAKE_OPTS) -C $(@D);
 endef
 
 define SJA1105_TOOL_INSTALL_TARGET_CMDS
