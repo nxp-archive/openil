d085032469d129d73e37ae4cf1b042acc76dece0
Liu Gang <Gang.Liu@nxp.com>
2017-06-08 14:10:00 +0800

    board:nxp: Update the NXP PPA firmware from 1701 to 1703
    Update the ls1012ardb, ls1043ardb, ls1046ardb platform
    PPA firmware from 1701 to 1703.
    
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>

--- board/nxp/ls1046ardb/ls1046ardb-32b.sh
+++ board/nxp/ls1046ardb/ls1046ardb-32b.sh
@@ -18,7 +18,7 @@ cp ${2}/tools/mkimage board/nxp/ls1046ardb/temp/
 # Copy the uboot mkimage to output/host/usr/bin for the PPA building
 cp ${2}/tools/mkimage output/host/usr/bin
 make ppa-build
-cp ${BUILD_DIR}/ppa-fsl-sdk-v2.0-1701/ppa/soc-ls1046/build/obj/ppa.itb board/nxp/ls1046ardb/temp/
+cp ${BUILD_DIR}/ppa-fsl-sdk-v2.0-1703/ppa/soc-ls1046/build/obj/ppa.itb board/nxp/ls1046ardb/temp/
 
 make clean
 rm .config
