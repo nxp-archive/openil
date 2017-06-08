9cc8f08fbb3673cbd112ed21e465d8ec077b81e0
Liu Gang <Gang.Liu@nxp.com>
2017-06-08 14:22:19 +0800

    board:ls1012ardb: Add 64bit system support for ls1012ardb
    Support ls1012ardb 64bit system with default OpenIL filesystem.
    
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>

--- board/nxp/ls1012ardb/post-image-64b.sh
+++ board/nxp/ls1012ardb/post-image-64b.sh
@@ -2,21 +2,25 @@
 # args from BR2_ROOTFS_POST_SCRIPT_ARGS
 # $2 linux building directory
 # $3 buildroot top directory
+# $4 uboot building directory
 
 main()
 {
+	echo ${2}
+	echo ${3}
+	echo ${4}
+
+	rm -rf board/nxp/ls1012ardb/temp
+	mkdir board/nxp/ls1012ardb/temp
+
+	# Copy the uboot mkimage to output/host/usr/bin for the PPA building
+	cp ${4}/tools/mkimage output/host/usr/bin
+
 	local MKIMAGE=${HOST_DIR}/usr/bin/mkimage
 
-	echo ${3}
-	echo ${2}
-
-	# cp the pre-build uboot mkimage to output/host/usr/bin
-	cp board/nxp/ls1012ardb/temp/mkimage output/host/usr/bin
-	rm -f board/nxp/ls1012ardb/temp/mkimage
-
-	# cp the pre-build uboot and dtb images to output/images
-	rm -f board/nxp/ls1012ardb/temp/Image
-	cp board/nxp/ls1012ardb/temp/* output/images/
+	# build the ppa firmware
+	make ppa-build
+	cp ${BUILD_DIR}/ppa-fsl-sdk-v2.0-1703/ppa/soc-ls1012/build/obj/ppa.itb output/images
 
 	# obtain the rcw image
 	tar zxf dl/rcw-fsl-sdk-v2.0-1701.tar.gz -C board/nxp/ls1012ardb/temp/
@@ -24,21 +28,20 @@ main()
 
 	# build the itb image
 	cp output/images/fsl-ls1012a-rdb.dtb ${2}/
-	cp ${BINARIES_DIR}/rootfs.ext2.gz ${2}/fsl-image-core-ls1012ardb-32b.ext2.gz
+	cp ${BINARIES_DIR}/rootfs.ext2.gz ${2}/fsl-image-core-ls1012ardb-20160616014215.rootfs.ext2.gz
 	cd ${2}/
-	${MKIMAGE} -f kernel-ls1012a-rdb-aarch32.its kernel-ls1012a-rdb-aarch32.itb
+	${MKIMAGE} -f kernel-ls1012a-rdb.its kernel-ls1012a-rdb.itb
 	cd ${3}
-	cp ${2}/kernel-ls1012a-rdb-aarch32.itb ${BINARIES_DIR}/
-	rm ${2}/fsl-image-core-ls1012ardb-32b.ext2.gz
+	cp ${2}/kernel-ls1012a-rdb.itb ${BINARIES_DIR}/
+	rm ${2}/fsl-image-core-ls1012ardb-20160616014215.rootfs.ext2.gz
 	rm ${2}/fsl-ls1012a-rdb.dtb
-	rm ${2}/kernel-ls1012a-rdb-aarch32.itb
-	ln -s ${BINARIES_DIR}/kernel-ls1012a-rdb-aarch32.itb ${BINARIES_DIR}/kernel-ls1012a-rdb.itb
+	rm ${2}/kernel-ls1012a-rdb.itb
 
 	# build the ramdisk rootfs
 	${MKIMAGE} -A arm -T ramdisk -C gzip -d ${BINARIES_DIR}/rootfs.ext2.gz ${BINARIES_DIR}/rootfs.ext2.gz.uboot
 
 	# build the SDcard image
-	local FILES=""kernel-ls1012a-rdb-aarch32.itb""
+	local FILES=""kernel-ls1012a-rdb.itb""
 	local GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
 	local GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
