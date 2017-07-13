fabe8e0077d79778ab50322cdac4df1c146ccfe7
Wenbin Song <wenbin.song@nxp.com>
2017-07-13 14:26:20 +0800

    package:python-wand: Add python wand package
    Some package's compilation like optee-os need
    the python-wand module.
    
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>
    Signed-off-by: Wenbin Song <wenbin.song@nxp.com>

---
+++ package/python-wand/python-wand.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-wand
+#
+################################################################################
+PYTHON_WAND_VERSION = 0.4.4
+PYTHON_WAND_SITE = git://github.com/dahlia/wand.git
+PYTHON_WAND_SITE_METHOD = git
+PYTHON_WAND_LICENSE = MIT
+PYTHON_WAND_LICENSE_FILES = LICENSE
+PYTHON_WAND_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
+$(eval $(host-python-package))
