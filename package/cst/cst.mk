f5ecfaea3cf881b77095aee9cc37c256d9e7fbf0
Liu Gang <Gang.Liu@nxp.com>
2017-07-19 15:28:16 +0800

    package:cst: Support to set core hold-off
    Add configurations to support setting core hold-off
    feature by make menuconfig.
    
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>

--- package/cst/cst.mk
+++ package/cst/cst.mk
@@ -10,6 +10,14 @@ CST_SITE_METHOD = git
 CST_LICENSE = NXP
 CST_LICENSE_FILES = COPYING
 
+ifneq ($(BR2_PACKAGE_HOST_CST_CORE_HOLDOFF),y)
+define CST_CORE_DISABLE_HOLDOFF
+	$(APPLY_PATCHES) $(@D) package/cst/uni_pbi/ 0001-uni_pbi-ls1-Disable-the-core-hold-off.patch
+endef
+
+HOST_CST_PRE_PATCH_HOOKS += CST_CORE_DISABLE_HOLDOFF
+endif
+
 # host build
 define HOST_CST_BUILD_CMDS
 	$(HOST_CONFIGURE_OPTS) $(MAKE) -C $(@D)
