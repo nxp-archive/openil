37332497b959a051dc47a36cdfd051b328451b10
Vladimir Oltean <vladimir.oltean@nxp.com>
2017-07-19 15:46:46 +0300

    yang-sja1105: cosmetic code cleanup
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- package/yang-sja1105/sja1105/sja1105.c
+++ package/yang-sja1105/sja1105/sja1105.c
@@ -1,7 +1,9 @@
 /*
  * This is automatically generated callbacks file
- * It contains 3 parts: Configuration callbacks, RPC callbacks and state data callbacks.
- * Do NOT alter function signatures or any structures unless you know exactly what you are doing.
+ * It contains 3 parts: Configuration callbacks, RPC callbacks and state
+ * data callbacks.
+ * Do NOT alter function signatures or any structures unless you know exactly
+ * what you are doing.
  */
 
 #include <stdio.h>
@@ -57,17 +59,20 @@ const char netconf_file_dir[] = "/etc/sja1105/";
  */
 const TRANSAPI_CLBCKS_ORDER_TYPE callbacks_order = TRANSAPI_CLBCKS_ORDER_DEFAULT;
 
-/* Do not modify or set! This variable is set by libnetconf to announce edit-config's error-option
-Feel free to use it to distinguish module behavior for different error-option values.
+/* Do not modify or set! This variable is set by libnetconf to announce
+ * edit-config's error-option. Feel free to use it to distinguish module
+ * behavior for different error-option values.
  * Possible values:
- * NC_EDIT_ERROPT_STOP - Following callback after failure are not executed, all successful callbacks executed till
+ * NC_EDIT_ERROPT_STOP - Following callback after failure are not executed,
+ *                       all successful callbacks executed till
                          failure point must be applied to the device.
- * NC_EDIT_ERROPT_CONT - Failed callbacks are skipped, but all callbacks needed to apply configuration changes are executed
- * NC_EDIT_ERROPT_ROLLBACK - After failure, following callbacks are not executed, but previous successful callbacks are
-                         executed again with previous configuration data to roll it back.
+ * NC_EDIT_ERROPT_CONT - Failed callbacks are skipped, but all callbacks
+ *                       needed to apply configuration changes are executed
+ * NC_EDIT_ERROPT_ROLLBACK - After failure, following callbacks are not executed,
+ *                       but previous successful callbacks are executed again
+ *                       with previous configuration data to roll it back.
  */
 NC_EDIT_ERROPT_TYPE erropt = NC_EDIT_ERROPT_NOTSET;
-
 
 char *sja1105_run_cmd(char *command_run)
 {
@@ -87,34 +92,36 @@ int write_to_datastore(char *file_name);
 int file_validate(char *file);
 
 /**
- * @brief Initialize plugin after loaded and before any other functions are called.
+ * @brief Initialize plugin after loaded and before any other functions
+ *        are called.
 
- * This function should not apply any configuration data to the controlled device. If no
- * running is returned (it stays *NULL), complete startup configuration is consequently
- * applied via module callbacks. When a running configuration is returned, libnetconf
- * then applies (via module's callbacks) only the startup configuration data that
- * differ from the returned running configuration data.
+ * This function should not apply any configuration data to the controlled
+ * device. If no running is returned (it stays *NULL), complete startup
+ * configuration is consequently applied via module callbacks. When a running
+ * configuration is returned, libnetconf then applies (via module's callbacks)
+ * only the startup configuration data that differ from the returned running
+ * configuration data.
 
- * Please note, that copying startup data to the running is performed only after the
- * libnetconf's system-wide close - see nc_close() function documentation for more
- * information.
+ * Please note, that copying startup data to the running is performed only
+ * after the libnetconf's system-wide close - see nc_close() function
+ * documentation for more information.
 
- * @param[out] running	Current configuration of managed device.
+ * @param[out] running  Current configuration of managed device.
 
  * @return EXIT_SUCCESS or EXIT_FAILURE
  */
-int transapi_init(xmlDocPtr *running) {
-
-	nc_verb_verbose("transapi_init: run sja1105-tool config load stanard_policy\n");
-
+int transapi_init(xmlDocPtr *running)
+{
+	nc_verb_verbose("transapi_init\n");
 	return EXIT_SUCCESS;
 }
 
 /**
- * @brief Free all resources allocated on plugin runtime and prepare plugin for removal.
+ * @brief Free all resources allocated on plugin runtime and prepare
+ *        plugin for removal.
  */
-void transapi_close(void) {
-
+void transapi_close(void)
+{
 	pthread_mutex_destroy(&file_mutex);
 	return;
 }
@@ -122,7 +129,7 @@ void transapi_close(void) {
 int save_to_temp_file(xmlNodePtr new_node)
 {
 	char command[256];
-	xmlDocPtr	configfile = xmlNewDoc(BAD_CAST "1.0");
+	xmlDocPtr configfile = xmlNewDoc(BAD_CAST "1.0");
 
 	xmlDocSetRootElement(configfile, new_node);
 	xmlSaveFormatFileEnc(tempxml, configfile, "UTF-8", 1);
@@ -139,15 +146,20 @@ int save_to_temp_file(xmlNodePtr new_node)
 	sja1105_run_cmd(command);
 	return 0;
 }
+
 /**
- * @brief Retrieve state data from device and return them as XML document
+ * @brief               Retrieve state data from device and return them
+ *                      as XML document
  *
- * @param model	Device data model. libxml2 xmlDocPtr.
- * @param running	Running datastore content. libxml2 xmlDocPtr.
- * @param[out] err  Double pointer to error structure. Fill error when some occurs.
- * @return State data as libxml2 xmlDocPtr or NULL in case of error.
+ * @param model         Device data model. libxml2 xmlDocPtr.
+ * @param running       Running datastore content. libxml2 xmlDocPtr.
+ * @param[out] err      Double pointer to error structure.
+ *                      Fill error when some occurs.
+ * @return              State data as libxml2 xmlDocPtr or NULL in case
+ *                      of error.
  */
-xmlDocPtr get_state_data(xmlDocPtr model, xmlDocPtr running, struct nc_err **err) {
+xmlDocPtr get_state_data(xmlDocPtr model, xmlDocPtr running, struct nc_err **err)
+{
 	nc_verb_verbose("get_state_data\n");
 	FILE *fp;
 	char cmd_xml_list[256];
@@ -158,14 +170,16 @@ xmlDocPtr get_state_data(xmlDocPtr model, xmlDocPtr running, struct nc_err **err
 	int i;
 
 	doc = xmlNewDoc(BAD_CAST "1.0");
-	xmlDocSetRootElement(doc, root = xmlNewDocNode(doc, NULL, BAD_CAST "sja1105", NULL));
+	xmlDocSetRootElement(doc, root = xmlNewDocNode(doc, NULL, BAD_CAST
+	                                               "sja1105", NULL));
 	ns = xmlNewNs(root, BAD_CAST SJA1105_NETCONF_NS, NULL);
 	xmlSetNs(root, ns);
 
 	xmlNodePtr node = xmlNewNode(NULL, BAD_CAST "config-files");
 	xmlAddChild(root, node);
 
-	sprintf(command_line, "ls -l %s/* | grep .xml | awk '{print $9}'", netconf_file_dir);
+	sprintf(command_line, "ls -l %s/* | grep .xml | awk '{print $9}'",
+	        netconf_file_dir);
 	if ((fp = popen(command_line, "r")) == NULL) {
 		printf("command fail: ls -l %s/*\n", netconf_file_dir);
 		return(NULL);
@@ -197,16 +211,13 @@ xmlDocPtr get_state_data(xmlDocPtr model, xmlDocPtr running, struct nc_err **err
 		int len = 0;
 
 		memset(full_status, 0, sizeof(full_status));
-
 		memset(command_line, 0, sizeof(command_line));
-
 		sprintf(command_line, "%s status ports %d", command_name, i);
 
 		if ((fp = popen(command_line, "r")) == NULL) {
 			printf("command fail: %s status ports %d\n", command_name, i);
 			return(NULL);
 		}
-
 		while (fgets(lbuf, 256, fp) != NULL) {
 			int llen;
 
@@ -215,12 +226,11 @@ xmlDocPtr get_state_data(xmlDocPtr model, xmlDocPtr running, struct nc_err **err
 			len += llen;
 			full_status[len++] = '\t';
 		}
-
 		xmlNewChild(node_ports, root->ns, BAD_CAST "port", BAD_CAST full_status);
 	}
-
 	return(doc);
 }
+
 /*
  * Mapping prefixes with namespaces.
  * Do NOT modify this structure!
@@ -229,23 +239,29 @@ struct ns_pair namespace_mapping[] = {{"nxp", SJA1105_NETCONF_NS}, {NULL, NULL}}
 
 /*
  * CONFIGURATION callbacks
- * Here follows set of callback functions run every time some change in associated part of running datastore occurs.
- * You can safely modify the bodies of all function as well as add new functions for better lucidity of code.
+ * Here follows set of callback functions run every time some change in
+ * associated part of running datastore occurs.
+ * You can safely modify the bodies of all function as well as add new
+ * functions for better lucidity of code.
  */
 
 /**
  * @brief This callback will be run when node in path /nxp:sja1105 changes
  *
- * @param[in] data	Double pointer to void. Its passed to every callback. You can share data using it.
- * @param[in] op	Observed change in path. XMLDIFF_OP type.
- * @param[in] old_node	Old configuration node. If op == XMLDIFF_ADD, it is NULL.
- * @param[in] new_node	New configuration node. if op == XMLDIFF_REM, it is NULL.
- * @param[out] error	If callback fails, it can return libnetconf error structure with a failure description.
+ * @param[in] data      Double pointer to void. Its passed to every callback.
+ *                      You can share data using it.
+ * @param[in] op        Observed change in path. XMLDIFF_OP type.
+ * @param[in] old_node  Old configuration node. If op == XMLDIFF_ADD, it is NULL.
+ * @param[in] new_node  New configuration node. if op == XMLDIFF_REM, it is NULL.
+ * @param[out] error    If callback fails, it can return libnetconf error
+ *                      structure with a failure description.
  *
  * @return EXIT_SUCCESS or EXIT_FAILURE
  */
 /* !DO NOT ALTER FUNCTION SIGNATURE! */
-int callback_nxp_sja1105(void **data, XMLDIFF_OP op, xmlNodePtr old_node, xmlNodePtr new_node, struct nc_err **error) {
+int callback_nxp_sja1105(void **data, XMLDIFF_OP op, xmlNodePtr old_node,
+                         xmlNodePtr new_node, struct nc_err **error)
+{
 	nc_verb_verbose("callback_nxp_sja1105\n");
 
 	int rc;
@@ -267,12 +283,12 @@ int callback_nxp_sja1105(void **data, XMLDIFF_OP op, xmlNodePtr old_node, xmlNod
 	} else {
 		nc_verb_verbose("unknown operation type\n");
 	}
-
 	return EXIT_SUCCESS;
 }
 
 /*
- * Structure transapi_config_callbacks provide mapping between callback and path in configuration datastore.
+ * Structure transapi_config_callbacks provide mapping between callback and
+ * path in configuration datastore.
  * It is used by libnetconf library to decide which callbacks will be run.
  * DO NOT alter this structure
  */
@@ -285,14 +301,16 @@ struct transapi_data_callbacks clbks =  {
 };
 
 /**
- * @brief Get a node from the RPC input. The first found node is returned, so if traversing lists,
- * call repeatedly with result->next as the node argument.
+ * @brief Get a node from the RPC input. The first found node is returned,
+ * so if traversing lists, call repeatedly with result->next as
+ * the node argument.
  *
  * @param name	Name of the node to be retrieved.
  * @param node	List of nodes that will be searched.
  * @return Pointer to the matching node or NULL
  */
-xmlNodePtr get_rpc_node(const char *name, const xmlNodePtr node) {
+xmlNodePtr get_rpc_node(const char *name, const xmlNodePtr node)
+{
 	xmlNodePtr ret = NULL;
 
 	for (ret = node; ret != NULL; ret = ret->next) {
@@ -300,18 +318,19 @@ xmlNodePtr get_rpc_node(const char *name, const xmlNodePtr node) {
 			break;
 		}
 	}
-
 	return ret;
 }
 
 /*
  * RPC callbacks
- * Here follows set of callback functions run every time RPC specific for this device arrives.
- * You can safely modify the bodies of all function as well as add new functions for better lucidity of code.
+ * Here follows set of callback functions run every time RPC specific for this
+ * device arrives.
+ * You can safely modify the bodies of all function as well as add new
+ * functions for better lucidity of code.
  * Every function takes an libxml2 list of inputs as an argument.
- * If input was not set in RPC message argument is set to NULL. To retrieve each argument, preferably use get_rpc_node().
+ * If input was not set in RPC message argument is set to NULL.
+ * To retrieve each argument, preferably use get_rpc_node().
  */
-
 
 xmlNodePtr probe_datastore_node(xmlNodePtr datastore_node, char *node_name)
 {
@@ -331,7 +350,6 @@ xmlNodePtr probe_datastore_node(xmlNodePtr datastore_node, char *node_name)
 			return node;
 		}
 	}
-
 out:
 	return NULL;
 }
@@ -346,7 +364,6 @@ int file_validate(char *file)
 		printf("config file not exist, newnodes_file= %s\n", newnodes_file);
 		return -1;
 	}
-
 	return 0;
 }
 
@@ -369,7 +386,7 @@ int write_to_datastore(char *file_name)
 
 	if(file_validate(file_name))
 		return -1;
-	
+
 	if (pthread_mutex_trylock(&file_mutex) != 0) {
 		/* file is still editing */
 		return -1;
@@ -385,8 +402,8 @@ int write_to_datastore(char *file_name)
 		return -1;
 	}
 
-	/* Init libxml */     
-    xmlInitParser();
+	/* Init libxml */
+	xmlInitParser();
 
 	xmlDocPtr doc_datastore = xmlReadFile(datastore_folder, NULL, 0);
 	xmlDocPtr doc_newnodes = xmlReadFile(newnodes_file, NULL, 0);
@@ -436,7 +453,7 @@ int write_to_datastore(char *file_name)
 	}
 
 	/* Add new sja1105 node to candidate node in datastore.xml */
-	xmlNodePtr rootnode_candidate = xmlCopyNodeList(root_newnodes);	
+	xmlNodePtr rootnode_candidate = xmlCopyNodeList(root_newnodes);
 
 	xmlSetProp(node_candidate, (const xmlChar *)"modified", (const xmlChar *)"true");
 
@@ -468,12 +485,12 @@ quiting:
 	xmlFreeDoc(doc_newnodes);
 
 	/* Shutdown libxml */
-    xmlCleanupParser();
-    
-    /*
-     * this is to debug memory for regression tests
-     */
-    xmlMemoryDump();
+	xmlCleanupParser();
+
+	/*
+	 * this is to debug memory for regression tests
+	 */
+	xmlMemoryDump();
 	pthread_mutex_unlock(&file_mutex);
 
 	return rc;
@@ -498,8 +515,8 @@ int sja1105_check_dir(char *file)
 	return 0;
 }
 
-nc_reply *rpc_save_local_config(xmlNodePtr input) {
-
+nc_reply *rpc_save_local_config(xmlNodePtr input)
+{
 	nc_verb_verbose("rpc_sja1105_config_save\n");
 	xmlNodePtr file_name_xml = get_rpc_node("configfile", input);
 	char *file_name;
@@ -523,12 +540,14 @@ nc_reply *rpc_save_local_config(xmlNodePtr input) {
 	ret = file_check(cmd_file);
 	if (ret < 0) {
 		memset(msg_err, '\0', sizeof(msg_err));
-		strcpy(msg_err, "file validate failure, input a name with .xml extended!");
+		strcpy(msg_err, "file validate failure, input a name with .xml extension!");
 		goto error;
 	}
-	nc_verb_verbose("rpc_sja1105_config_save: preparing save file : %s\n", cmd_file);
+	nc_verb_verbose("rpc_sja1105_config_save: preparing save file : %s\n",
+	                cmd_file);
 
-	sprintf(command_full, "%s config save %s/%s", command_name, conf_folder, cmd_file);
+	sprintf(command_full, "%s config save %s/%s",
+	        command_name, conf_folder, cmd_file);
 
 	sja1105_run_cmd(command_full);
 
@@ -542,7 +561,8 @@ error:
 	return nc_reply_error(e);
 }
 
-nc_reply *rpc_load_local_config(xmlNodePtr input) {
+nc_reply *rpc_load_local_config(xmlNodePtr input)
+{
 	nc_verb_verbose("rpc_sja1105_config_load\n");
 	xmlNodePtr file_name_xml = get_rpc_node("configfile", input);
 	char *file_name;
@@ -554,9 +574,7 @@ nc_reply *rpc_load_local_config(xmlNodePtr input) {
 	char msg_err[256];
 
 	file_name = (char*)xmlNodeGetContent(file_name_xml);
-
 	sscanf(file_name, "%s", cmd_file);
-
 	free(file_name);
 
 	if (cmd_file[0] == '\"')
@@ -573,7 +591,8 @@ nc_reply *rpc_load_local_config(xmlNodePtr input) {
 		goto error;
 	}
 
-	nc_verb_verbose("rpc_sja1105_config_load: preparing load file : %s\n", cmd_file);
+	nc_verb_verbose("rpc_sja1105_config_load: preparing load file: %s\n",
+	                cmd_file);
 
 	sprintf(folder, "%s/%s", conf_folder, cmd_file);
 
@@ -584,7 +603,8 @@ nc_reply *rpc_load_local_config(xmlNodePtr input) {
 		goto error;
 	}
 
-	sprintf(command_full, "%s config load -f %s/%s", command_name, conf_folder, cmd_file);
+	sprintf(command_full, "%s config load -f %s/%s",
+	        command_name, conf_folder, cmd_file);
 
 	sja1105_run_cmd(command_full);
 
@@ -594,16 +614,15 @@ nc_reply *rpc_load_local_config(xmlNodePtr input) {
 		strcpy(msg_err, "run netconf xml file load failure");
 		goto error;
 	}
-
 	return nc_reply_ok();
-
 error:
 	e = nc_err_new(NC_ERR_IN_USE);
 	nc_err_set(e, NC_ERR_PARAM_MSG, msg_err);
 	return nc_reply_error(e);
 }
 
-nc_reply *rpc_load_default(xmlNodePtr input) {
+nc_reply *rpc_load_default(xmlNodePtr input)
+{
 	nc_verb_verbose("rpc_sja1105_config_default\n");
 	char command_full[] = "sja1105-tool config default -f ls1021atsn";
 	int ret;
@@ -618,7 +637,6 @@ nc_reply *rpc_load_default(xmlNodePtr input) {
 		strcpy(msg_err, "run netconf xml file load failure");
 		goto default_error;
 	}
-
 	return nc_reply_ok();
 
 default_error:
