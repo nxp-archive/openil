15e962c638cd8b4bcacc5d3f86cdfe9705a93a56
Vladimir Oltean <vladimir.oltean@nxp.com>
2017-12-11 03:05:32 +0200

    prl: restructure time accounting, accept any submultiples of second on --every
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- package/prl/src/prl.c
+++ package/prl/src/prl.c
@@ -6,15 +6,17 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <sys/time.h>
 #include <time.h>
 
-#define NSEC_PER_SEC  1000000000L
+#define NSEC_PER_USEC 1000L
 #define NSEC_PER_MSEC 1000000L
+#define NSEC_PER_SEC  1000000000L
 
 enum prl_opt {
 	PRL_OPT_COUNT = 0,
-	PRL_OPT_EVERY_MS,
+	PRL_OPT_EVERY,
 	PRL_OPT_HELP,
 	PRL_OPT_NULL
 };
@@ -26,10 +28,10 @@ struct option prl_opts[] = {
 		.flag = NULL,
 		.val = PRL_OPT_COUNT
 	},
-	[PRL_OPT_EVERY_MS] = {
-		.name = "every-ms",
+	[PRL_OPT_EVERY] = {
+		.name = "every",
 		.has_arg = 1,
-		.val = PRL_OPT_EVERY_MS
+		.val = PRL_OPT_EVERY
 	},
 	[PRL_OPT_HELP] = {
 		.name = "help",
@@ -38,6 +40,33 @@ struct option prl_opts[] = {
 	},
 	[PRL_OPT_NULL] = { NULL, 0, NULL, 0 }
 };
+
+int reliable_double_from_string(double *to, char *from, char **endptr)
+{
+	int   errno_saved = errno;
+	int   rc = 0;
+	char *p;
+
+	errno = 0;
+	*to = strtod(from, &p);
+	if (endptr != NULL) {
+		*endptr = p;
+	}
+	if (errno) {
+		fprintf(stderr, "Range error occured while reading \"%s\"", from);
+		rc = -errno;
+		goto out;
+	}
+	if (from == p) {
+		/* Read nothing */
+		fprintf(stderr, "No double stored at \"%s\"", from);
+		rc = -errno;
+		goto out;
+	}
+out:
+	errno = errno_saved;
+	return rc;
+}
 
 int reliable_uint64_from_string(uint64_t *to, char *from, char **endptr)
 {
@@ -71,44 +100,58 @@ out:
 	return rc;
 }
 
-int in_contract(struct timespec current_time,
-                struct timespec last_time,
-                uint64_t every_ms,
-                uint64_t count,
-                uint64_t *printed_lines)
+int64_t ns_from_timespec(struct timespec t)
 {
-	uint64_t elapsed_ns = (current_time.tv_sec - last_time.tv_sec) * NSEC_PER_SEC +
-	                      (current_time.tv_nsec - last_time.tv_nsec);
-	if (*printed_lines >= count) {
-		return false;
-	}
-	if (elapsed_ns < every_ms * NSEC_PER_MSEC) {
-		return false;
-	}
-	(*printed_lines)++;
-	return true;
+	return (t.tv_sec * NSEC_PER_SEC) + t.tv_nsec;
 }
 
-void main_loop(uint64_t every_ms, uint64_t count)
+void main_loop(uint64_t every_ns, uint64_t count)
 {
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t read;
 	struct timespec current_time;
 	struct timespec last_time;
+	int64_t current_time_ns;
+	int64_t last_time_ns;
 	uint64_t printed_lines = 0;
+	bool in_contract;
 
+	setlinebuf(stdin);
 	clock_gettime(CLOCK_MONOTONIC, &last_time);
-	while ((read = getline(&line, &len, stdin)) != -1) {
+	last_time_ns = ns_from_timespec(last_time);
+	/* Round to closest multiple of every_ns */
+	last_time_ns /= every_ns;
+	last_time_ns *= every_ns;
+	while (getline(&line, &len, stdin) != -1) {
 		clock_gettime(CLOCK_MONOTONIC, &current_time);
-		if (in_contract(current_time, last_time, every_ms, count, &printed_lines)) {
+		current_time_ns = ns_from_timespec(current_time);
+		if (current_time_ns - last_time_ns < every_ns) {
+			/* Spoke too soon?
+			 * Only if not too much.
+			 */
+			in_contract = (printed_lines < count);
+		} else {
+			/* At least a full every_ns interval has
+			 * passed since last line */
+			in_contract = true;
+			/* Reset the "spoke too much" counter for a
+			 * new time interval */
+			printed_lines = 0;
+			/* Increment last_time_ns to closest every_ns multiple
+			 * that is not larger than current_time_ns
+			 */
+			last_time_ns = current_time_ns;
+			last_time_ns /= every_ns;
+			last_time_ns *= every_ns;
+		}
+		if (in_contract) {
 			printf("%s", line);
 			fflush(stdout);
-			last_time = current_time;
-			printed_lines = 0;
+			printed_lines++;
 		}
 	}
 	free(line);
+	fclose(stdin);
 }
 
 void usage()
@@ -118,7 +161,7 @@ void usage()
 	       "dropping excess lines.\n"
 	       "It implements a very crude accounting mechanism similar in nature to a\n"
 	       "token bucket filter. As long as lines received on stdin are in contract\n"
-	       "(no more than \"--count\" lines on \"--every-ms\" millisecond period),\n"
+	       "(no more than \"--count\" lines on \"--every\" time period),\n"
 	       "lines on stdin are reproduced per-se on stdout. Stdin lines in excess\n"
 	       "of the contract are simply dropped.\n"
 	       "\n"
@@ -130,17 +173,21 @@ void usage()
 	       "to consume time from its event loop just to discard it.\n"
 	       "PRL acts as a protection for such slow consumer programs.\n"
 	       "\n"
+	       "Running PRL makes sense only when the producer program generates\n"
+	       "line-buffered output (tail -f, tcpdump -l).\n"
 	       "Usage:\n"
-	       "prl --count <number of lines> --every-ms <integer>\n");
+	       "prl --count <number of lines> --every <integer>\n");
 }
 
 int main(int argc, char **argv)
 {
 	int opt, opt_index;
-	char *opt_every_ms = NULL;
+	char *opt_every = NULL;
 	char *opt_count = NULL;
-	uint64_t every_ms;
+	char *unit_of_measure;
+	double   every;
 	uint64_t count;
+	uint64_t every_ns;
 
 	while (1) {
 		opt = getopt_long(argc, argv, "+c:e:h", prl_opts, &opt_index);
@@ -153,12 +200,12 @@ int main(int argc, char **argv)
 			usage();
 			return 0;
 		case 'e':
-		case PRL_OPT_EVERY_MS:
-			if (opt_every_ms) {
-				fprintf(stderr, "--every-ms cannot be provided twice!\n");
+		case PRL_OPT_EVERY:
+			if (opt_every) {
+				fprintf(stderr, "--every cannot be provided twice!\n");
 				return 1;
 			}
-			opt_every_ms = optarg;
+			opt_every = optarg;
 			break;
 		case 'c':
 		case PRL_OPT_COUNT:
@@ -181,8 +228,8 @@ int main(int argc, char **argv)
 			printf("%s ", argv[optind++]);
 		printf("\n");
 	}
-	if (!opt_every_ms) {
-		fprintf(stderr, "Please supply a millisecond value to --every-ms\n");
+	if (!opt_every) {
+		fprintf(stderr, "Please supply a time value to --every (1s, 0.5ms, 3.8us, 5ns)\n");
 		return 1;
 	}
 	if (!opt_count) {
@@ -190,15 +237,28 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	if (reliable_uint64_from_string(&every_ms, opt_every_ms, NULL) < 0) {
-		fprintf(stderr, "Expected integer for --every-ms parameter.\n");
+	if (reliable_double_from_string(&every, opt_every, &unit_of_measure) < 0) {
+		fprintf(stderr, "Expected integer for --every parameter.\n");
+		goto out;
+	}
+	if (strlen(unit_of_measure) == 0 || strcmp(unit_of_measure, "s") == 0) {
+		every_ns = every * NSEC_PER_SEC;
+	} else if (strcmp(unit_of_measure, "ms") == 0) {
+		every_ns = every * NSEC_PER_MSEC;
+	} else if (strcmp(unit_of_measure, "us") == 0) {
+		every_ns = every * NSEC_PER_USEC;
+	} else if (strcmp(unit_of_measure, "ns") == 0) {
+		every_ns = every;
+	} else {
+		fprintf(stderr, "unexpected unit of measurement \"%s\". "
+		                "expected s, ms, us or ns.\n", unit_of_measure);
 		goto out;
 	}
 	if (reliable_uint64_from_string(&count, opt_count, NULL) < 0) {
 		fprintf(stderr, "Expected integer for --count parameter.\n");
 		goto out;
 	}
-	main_loop(every_ms, count);
+	main_loop(every_ns, count);
 out:
 	return 0;
 }
