a14eb49ae9dfdb0ba31cab08ddb4762cdc950e44
Liu Gang <Gang.Liu@nxp.com>
2018-03-30 17:43:45 +0800

    package:baremetal: Add baremetal package based on Uboot
    For some multiple cores silicon and AMP system, one or more
    cores can run baremetal software firmware in order to get
    better performance.
    
    The baremetal package is created based on Uboot project, and
    can be deployed on one or more cores.
    
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>

---
+++ package/bm-uboot/bm-uboot.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# Baremetal OS framework based on Uboot
+#
+################################################################################
+
+BM_UBOOT_VERSION = OpenIL-Baremetal-201803
+BM_UBOOT_SITE = https://github.com/openil/u-boot.git
+BM_UBOOT_SITE_METHOD = git
+BM_UBOOT_LICENSE = GPL-2.0+
+BM_UBOOT_LICENSE_FILES = README
+BM_UBOOT_DEPENDENCIES += host-dtc
+
+BM_UBOOT_MAKE_OPTS = \
+	CC="$(TARGET_CC)" \
+	CROSS_COMPILE="$(TARGET_CROSS)" \
+
+BM_UBOOT_DEFCONFIG = $(call qstrip,$(BR2_PACKAGE_BM_UBOOT_DEFCONFIG))
+
+define BM_UBOOT_BUILD_CMDS
+	cd $(@D)/ && $(MAKE) distclean
+	cd $(@D)/ && $(MAKE) $(BM_UBOOT_DEFCONFIG)
+	$(TARGET_MAKE_ENV) $(MAKE) $(BM_UBOOT_MAKE_OPTS)-C $(@D)
+endef
+
+define BM_UBOOT_INSTALL_TARGET_CMDS
+	cp -dpfr $(@D)/u-boot.bin $(BINARIES_DIR)/bm-u-boot.bin
+endef
+
+$(eval $(generic-package))
