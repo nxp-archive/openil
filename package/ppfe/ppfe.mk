0b60039125edace48803df89e28b78b93a73e964
Vladimir Oltean <vladimir.oltean@nxp.com>
2018-08-28 19:59:51 +0300

    package:ppfe: update remote to NXP Github
    * The ppfe repository was removed from git.freescale.com
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- package/ppfe/ppfe.mk
+++ package/ppfe/ppfe.mk
@@ -4,14 +4,15 @@
 #
 ################################################################################
 
-PPFE_VERSION = fsl-sdk-v2.0-1701
-PPFE_SITE = git://git.freescale.com/ppc/sdk/engine-pfe-bin.git
-PPFE_LICENSE = Freescale-Binary-EULA
-PPFE_LICENSE_FILES = Freescale-Binary-EULA
+PPFE_VERSION = integration
+PPFE_SITE = https://github.com/NXP/qoriq-engine-pfe-bin.git
+PPFE_SITE_METHOD = git
+PPFE_LICENSE = NXP-Binary-EULA.txt
+PPFE_LICENSE_FILES = NXP-Binary-EULA.txt
 
 define PPFE_INSTALL_TARGET_CMDS
 $(INSTALL) -d $(TARGET_DIR)/lib/firmware
-$(INSTALL) -m 0644 $(@D)/Freescale-Binary-EULA $(TARGET_DIR)/lib/firmware
+$(INSTALL) -m 0644 $(@D)/NXP-Binary-EULA.txt $(TARGET_DIR)/lib/firmware
 $(INSTALL) -m 0755 $(@D)/ls1012a/slow_path/*.elf $(TARGET_DIR)/lib/firmware
 endef
 
