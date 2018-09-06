d10d2ae26a12232228941241a1c4cfaa4f74eb89
Changming Huang <jerry.huang@nxp.com>
2018-09-06 20:00:03 +0800

    OpenIL: Enable LS1012ARDB board
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

--- package/ppfe/ppfe.mk
+++ package/ppfe/ppfe.mk
@@ -6,13 +6,14 @@
 
 PPFE_VERSION = LSDK-18.06
 PPFE_SITE = https://github.com/NXP/qoriq-engine-pfe-bin.git
-PPFE_LICENSE = Freescale-Binary-EULA
-PPFE_LICENSE_FILES = Freescale-Binary-EULA
+PPFE_SITE_METHOD = git
+PPFE_LICENSE = NXP-Binary-EULA
+PPFE_LICENSE_FILES = NXP-Binary-EULA
 
 define PPFE_INSTALL_TARGET_CMDS
 $(INSTALL) -d $(TARGET_DIR)/lib/firmware
-$(INSTALL) -m 0644 $(@D)/Freescale-Binary-EULA $(TARGET_DIR)/lib/firmware
 $(INSTALL) -m 0755 $(@D)/ls1012a/slow_path/*.elf $(TARGET_DIR)/lib/firmware
+cp -f $(@D)/ls1012a/u-boot/pfe_fw_sbl.itb $(BINARIES_DIR)
 endef
 
 $(eval $(generic-package))
