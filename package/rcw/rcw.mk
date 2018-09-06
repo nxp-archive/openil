dc17846f99f5b3c49cab93a9170a3d5ced96acf4
Changming Huang <jerry.huang@nxp.com>
2018-09-06 20:24:51 +0800

    OpenIL/RCW: build the rcw for ls1012ardb
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

--- package/rcw/rcw.mk
+++ package/rcw/rcw.mk
@@ -14,12 +14,10 @@ RCW_BIN = $(call qstrip,$(BR2_PACKAGE_RCW_BIN))
 RCW_PLATFORM = $(firstword $(subst /, ,$(RCW_BIN)))
 
 define RCW_BUILD_CMDS
-	if [ $(RCW_PLATFORM) != ls1012ardb ]; then \
-		cd $(@D)/$(RCW_PLATFORM) && $(MAKE); \
-	fi
-	if [ $(RCW_PLATFORM) = ls1046ardb ]; then \
-		tclsh board/nxp/common/byte_swap.tcl $(@D)/$(RCW_BIN) $(@D)/$(RCW_BIN).swap 8; \
-		cp -f $(@D)/$(RCW_BIN).swap $(BINARIES_DIR); \
+	cd $(@D)/$(RCW_PLATFORM) && $(MAKE); \
+	if [ $(RCW_PLATFORM) = ls1046ardb ] || [ $(RCW_PLATFORM) = ls1012ardb ]; then \
+		tclsh board/nxp/common/byte_swap.tcl $(@D)/$(RCW_BIN) $(@D)/$(RCW_BIN) 8; \
+		cp -f $(@D)/$(RCW_BIN).swapped $(BINARIES_DIR); \
 	else \
 		cp -f $(@D)/$(RCW_BIN) $(BINARIES_DIR); \
 	fi
