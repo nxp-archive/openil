16ce1693269526cf7853cac1d415f778ac04b022
Changming Huang <jerry.huang@nxp.com>
2019-01-10 15:01:36 +0800

    ls1028ardb/dp: display port support
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

---
+++ package/dp/dp.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# DP firmware for NXP layerscape platforms
+#
+################################################################################
+
+DP_BIN = $(call qstrip,$(BR2_PACKAGE_DP_BIN))
+ 
+define DP_BUILD_CMDS
+	cd $(@D)/ && wget https://www.nxp.com/lgfiles/sdk/ls1028a_bsp_01/ls1028a-dp-fw.bin &&\
+	chmod +x ls1028a-dp-fw.bin && ./ls1028a-dp-fw.bin --auto-accept && \
+	cp -f ls1028a-dp-fw/cadence/$(DP_BIN) $(BINARIES_DIR)/ls1028a-dp-fw.bin;
+endef
+
+$(eval $(generic-package))
