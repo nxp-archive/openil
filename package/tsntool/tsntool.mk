d4e4c226fb6b4000a766250421ef1ed02d4d99dd
Changming Huang <jerry.huang@nxp.com>
2019-01-11 14:49:12 +0800

    openil/tsntool: change the repos to github
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

--- package/tsntool/tsntool.mk
+++ package/tsntool/tsntool.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TSNTOOL_VERSION = v0.1
-TSNTOOL_SITE = ssh://git@bitbucket.sw.nxp.com/dnind/tsntool.git
+TSNTOOL_SITE = https://github.com/openil/tsntool.git
 TSNTOOL_SITE_METHOD = git
 TSNTOOL_LICENSE = MIT/GPL2.0
 TSNTOOL_LICENSE_FILES = LICENSE
@@ -13,7 +13,7 @@ TSNTOOL_INSTALL_STAGING = YES
 TSNTOOL_DEPENDENCIES = libnl readline ncurses
 
 define TSNTOOL_BUILD_CMDS
-	mkdir $(@D)/include/linux/;
+	mkdir -p $(@D)/include/linux/;
 	$(SED) 's/termcap/ncurses/' $(@D)/Makefile;
 	$(INSTALL) -D -m 0755 $(BUILD_DIR)/linux-$(BR2_LINUX_KERNEL_CUSTOM_REPO_VERSION)/include/uapi/linux/tsn.h $(@D)/include/linux/;
 	$(TARGET_CONFIGURE_ARGS) $(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) $(TARGET_MAKE_OPTS) -C $(@D)
