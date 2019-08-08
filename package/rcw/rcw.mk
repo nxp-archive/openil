ac6a524c48c0149607bc7258abccaa1b803f16c9
Jianchao Wang <jianchao.wang@nxp.com>
2019-08-08 18:50:28 +0800

    RCW: add two patchs for LS1028ARDB
    Signed-off-by: Jianchao Wang <jianchao.wang@nxp.com>

--- package/rcw/rcw.mk
+++ package/rcw/rcw.mk
@@ -13,6 +13,14 @@ RCW_LICENSE_FILES = LICENSE
 RCW_BIN = $(call qstrip,$(BR2_PACKAGE_RCW_BIN))
 RCW_PLATFORM = $(firstword $(subst /, ,$(RCW_BIN)))
 
+FIND = ls1028ardb
+ifeq ($(findstring $(FIND), $(BR2_TARGET_UBOOT_BOARDNAME)), $(FIND))
+define RCW_CONFIGURE_CMDS
+	patch -p1 -s -d $(@D) < $(TARGET_DIR)/../../package/rcw/0001-ls1028ardb-Enable-IIC5_PMUX-for-GPIO-function.patch.conditional;\
+	patch -p1 -s -d $(@D) < $(TARGET_DIR)/../../package/rcw/0002-ls1028ardb-Enable-CLK_OUT_PMUX-for-GPIO-function.patch.conditional
+endef
+endif
+
 define RCW_BUILD_CMDS
 	cd $(@D)/$(RCW_PLATFORM) && $(MAKE); \
 	if [ $(RCW_PLATFORM) = ls1046ardb ] || [ $(RCW_PLATFORM) = ls1012ardb ]; then \
