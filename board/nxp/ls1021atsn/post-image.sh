5628051620ea9f8985fc4e4eb8416992ef926503
Vladimir Oltean <olteanv@gmail.com>
2019-09-16 14:54:38 +0300

    board: nxp: ls1021a-tsn: Add support for generating an extlinux file
    This is used by boot firmware that complies to distro boot rules, such
    as U-Boot
    (https://gitlab.denx.de/u-boot/u-boot/blob/master/doc/README.distro).
    
    So make OpenIL a distribution that is compatible with distro booting.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- board/nxp/ls1021atsn/post-image.sh
+++ board/nxp/ls1021atsn/post-image.sh
@@ -28,6 +28,32 @@ linux_image()
 	fi
 }
 
+gen_extlinux()
+{
+	local in="board/nxp/common/extlinux.conf"
+	local out="${BINARIES_DIR}/extlinux.conf"
+	local kernel=
+	local dtbs=
+
+	if grep -Eq "^BR2_LINUX_KERNEL_UIMAGE=y$" ${BR2_CONFIG}; then
+		kernel="../uImage"
+	else
+		kernel="../zImage"
+	fi
+
+	for dt in "$(sed -n 's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\([a-z0-9 \-]*\)"$/\1/p' ${BR2_CONFIG})"; do
+		# Discard any dtb in the list except the last
+		dtb="../$dt.dtb"
+	done
+
+	cp -f "${in}" "${out}"
+	sed -i -e "s|%KERNEL%|${kernel}|g" \
+		-e "s|%DTB%|${dtb}|g" \
+		-e "s|%VERSION%|${BR2_OPENIL_VERSION}|g" \
+		-e "s|%BOARD%|NXP LS1021A-TSN|g" \
+		"${out}"
+}
+
 main()
 {
 	mkimage -A arm -T ramdisk -C gzip -d ${BINARIES_DIR}/rootfs.ext2.gz ${BINARIES_DIR}/rootfs.ext2.gz.uboot
@@ -39,6 +65,8 @@ main()
 	local FILES="$(dtb_list) $(linux_image), "version.json", "rootfs.ext2.gz.uboot""
 	local GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
 	local GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+	gen_extlinux
 
 	sed -e "s/%FILES%/${FILES}/" \
 		board/nxp/ls1021atsn/genimage.cfg.template > ${GENIMAGE_CFG}
