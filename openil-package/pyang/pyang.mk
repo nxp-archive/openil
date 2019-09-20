0751a3b0ae0616155f790c56f64dffaa316ad176
Vladimir Oltean <olteanv@gmail.com>
2019-09-20 13:59:29 +0300

    openil-package/pyang: Delete PYANG_INSTALL_STAGING_CMDS
    This serves no purpose.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- openil-package/pyang/pyang.mk
+++ openil-package/pyang/pyang.mk
@@ -14,11 +14,5 @@ PYANG_SETUP_TYPE = setuptools
 HOST_PYANG_DEPENDENCIES = host-python-lxml host-libxslt
 PYANG_DEPENDENCIES = python-lxml python
 
-define PYANG_INSTALL_STAGING_CMDS
-	export PYTHONPATH=$(HOST_DIR)/usr/lib/python2.7/site-packages
-	cd $(@D);$(HOST_DIR)/usr/bin/python $(@D)/setup.py install --prefix=$(HOST_DIR)/usr
-endef
-
 $(eval $(python-package))
 $(eval $(host-python-package))
-
