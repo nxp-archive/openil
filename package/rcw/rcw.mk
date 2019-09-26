30c74819dfe71d80b1eb287b07a28be06a25a1a4
Jerry Huang <jerry.huang@nxp.com>
2019-09-26 15:32:02 +0800

    package/rcw: change the repos to dash-rcw
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

--- package/rcw/rcw.mk
+++ package/rcw/rcw.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RCW_VERSION = ls1028a-early-access
+RCW_VERSION = LSDK-19.09
 RCW_SITE = https://source.codeaurora.org/external/qoriq/qoriq-components/rcw
 RCW_SITE_METHOD = git
 RCW_LICENSE = BSD License
@@ -23,12 +23,7 @@ endif
 
 define RCW_BUILD_CMDS
 	cd $(@D)/$(RCW_PLATFORM) && $(MAKE); \
-	if [ $(RCW_PLATFORM) = ls1046ardb ] || [ $(RCW_PLATFORM) = ls1012ardb ]; then \
-		tclsh board/nxp/common/byte_swap.tcl $(@D)/$(RCW_BIN) $(@D)/$(RCW_BIN) 8; \
-		cp -f $(@D)/$(RCW_BIN).swapped $(BINARIES_DIR); \
-	else \
-		cp -f $(@D)/$(RCW_BIN) $(BINARIES_DIR); \
-	fi
+	cp -f $(@D)/$(RCW_BIN) $(BINARIES_DIR);
 endef
 
 $(eval $(generic-package))
