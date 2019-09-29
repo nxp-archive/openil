nxp-archive/openil

commit 2ec6b4642edaa4f4278315e6366c3120a26d04df

Move OpenIL packages to separate folder
Try to achieve the following goals:
- Easier pulling for the common Buildroot base
- Sending any Buildroot patch upstream
- Keeping the OpenIL-specific packages separately from the rest

Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

openil-package/aliyun/aliyun.mk
@@ -1,0 +1,28 @@
+################################################################################
+#
+# Aliyun iot gateway sdk for arm-linux platforms
+#
+################################################################################
+
+ALIYUN_VERSION = master
+ALIYUN_SITE = https://@bitbucket.sw.nxp.com/scm/dnind/aliyun-iotkit-embedded.git
+ALIYUN_SITE_METHOD = git
+ALIYUN_LICENSE = Apache-2.0
+ALIYUN_LICENSE_FILES = LICENSE
+
+ALIYUN_MAKE_OPTS = \
+	OVERRIDE_CC="$(TARGET_CC)" \
+	OVERRIDE_AR="$(TARGET_AR)" \
+
+define ALIYUN_BUILD_CMDS
+	cd $(@D)/ && $(MAKE) distclean
+	$(TARGET_MAKE_ENV) $(MAKE) $(ALIYUN_MAKE_OPTS) -C $(@D)
+endef
+
+define ALIYUN_INSTALL_TARGET_CMDS
+	cp -dpfr $(@D)/output/release/bin/* $(TARGET_DIR)/usr/sbin/
+	cp -dpfr $(@D)/exec/* $(TARGET_DIR)/usr/sbin/ 2>/dev/null; true
+	cp -dpfr package/aliyun/device/*.json $(TARGET_DIR)/usr/sbin/ 2>/dev/null; true
+endef
+
+$(eval $(generic-package))

commit a5dc033ac0ad044a246b681977f8800e1f69264c

openil-package: Replace all open-coded uses of "package/"
Commit "Move OpenIL packages to separate folder" has created some build
errors, due to the fact that packages were not agnostic of their recipe
folder location. Use the correct _PKGDIR Buildroot variable to fix this.

Fixes: 2ec6b4642eda ("Move OpenIL packages to separate folder")
Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

openil-package/aliyun/aliyun.mk
@@ -22,7 +22,7 @@ endef
 define ALIYUN_INSTALL_TARGET_CMDS
 	cp -dpfr $(@D)/output/release/bin/* $(TARGET_DIR)/usr/sbin/
 	cp -dpfr $(@D)/exec/* $(TARGET_DIR)/usr/sbin/ 2>/dev/null; true
-	cp -dpfr package/aliyun/device/*.json $(TARGET_DIR)/usr/sbin/ 2>/dev/null; true
+	cp -dpfr $(ALIYUN_PKGDIR)/device/*.json $(TARGET_DIR)/usr/sbin/ 2>/dev/null; true
 endef
 
 $(eval $(generic-package))

openil-package/icc/icc.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ICC_VERSION = 0.1
-ICC_SITE = package/icc/src
+ICC_SITE = $(ICC_PKGDIR)/src
 ICC_SITE_METHOD = local
 ICC_LICENSE = GPL
 ifdef BR2_PACKAGE_ICC_GIC_OFFSET_ALIGN

openil-package/nodejs-lbt/nodejs-lbt.mk
@@ -1,5 +1,5 @@
 NODEJS_LBT_VERSION       := 0.2.0
-NODEJS_LBT_SITE          := package/nodejs-lbt/src
+NODEJS_LBT_SITE          := $(NODEJS_LBT_PKGDIR)/src
 NODEJS_LBT_SITE_METHOD   := local
 NODEJS_LBT_LICENSE       := MIT
 NODEJS_LBT_LICENSE_FILES := LICENSE

openil-package/opc-motor-status/opc-motor-status.mk
@@ -1,5 +1,5 @@
 OPC_MOTOR_STATUS_VERSION       := 0.0.1
-OPC_MOTOR_STATUS_SITE          := package/opc-motor-status/src
+OPC_MOTOR_STATUS_SITE          := $(OPC_MOTOR_STATUS_PKGDIR)/src
 OPC_MOTOR_STATUS_SITE_METHOD   := local
 OPC_MOTOR_STATUS_LICENSE       := BSD-3c
 OPC_MOTOR_STATUS_LICENSE_FILES := COPYING

openil-package/prl/prl.mk
@@ -1,5 +1,5 @@
 PRL_VERSION = 0.1
-PRL_SITE = package/prl/src
+PRL_SITE = $(PRL_PKGDIR)/src
 PRL_SITE_METHOD = local
 PRL_LICENSE = BSD-3c
 PRL_LICENSE_FILES = COPYING
@@ -13,4 +13,3 @@ define PRL_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(generic-package))
-

openil-package/transapi/transapi.mk
@@ -70,13 +70,13 @@ define TRANSAPI_CREATE_CONFIGURE
 		--search-path $(@D)/$(BRIDGE_SRC_NAME)/ \
 		transapi;
 
-        cd $(TOPDIR); \
-        $(APPLY_PATCHES) $(@D) package/transapi\
+	cd $(TOPDIR); \
+	$(APPLY_PATCHES) $(@D) $(TRANSAPI_PKGDIR) \
 		0001-modify-configure.in-to-change-parameters-to-fit-the-.patch.conditional; \
-        $(APPLY_PATCHES) $(@D) package/transapi\
+	$(APPLY_PATCHES) $(@D) $(TRANSAPI_PKGDIR) \
 		0002-modify-Makefile.in-to-change-the-project.patch.conditional; \
-        cd $(@D); \
-        $(TARGET_MAKE_ENV) $(HOST_DIR)/usr/bin/autoreconf --force --install
+	cd $(@D); \
+	$(TARGET_MAKE_ENV) $(HOST_DIR)/usr/bin/autoreconf --force --install
 endef
 
 # create Makefile

openil-package/tsntool/tsntool.mk
@@ -22,7 +22,7 @@ endef
 define TSNTOOL_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libtsn.so $(TARGET_DIR)/usr/lib/;
 	$(INSTALL) -D -m 0755 $(@D)/tsntool $(TARGET_DIR)/usr/bin/;
-	cp -rf package/tsntool/samples/ $(TARGET_DIR)/root
+	cp -rf $(TSNTOOL_PKGDIR)/samples/ $(TARGET_DIR)/root
 endef
 
 define TSNTOOL_INSTALL_STAGING_CMDS