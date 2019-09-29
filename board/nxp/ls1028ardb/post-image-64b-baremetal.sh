nxp-archive/openil

commit 675d23bd6bf69e82f03554af740882a0faaacd04

ls1028ardb: unify the RCW file for SD boot and eMMC boot
Signed-off-by: Changming Huang <jerry.huang@nxp.com>

board/nxp/ls1028ardb/post-image-64b.sh
@@ -11,6 +11,10 @@ main()
 	echo ${4}
 	echo ${BR2_ROOTFS_PARTITION_SIZE}
 
+	local RCWFILE=${BR2_PACKAGE_RCW_BIN##*/}
+	RCWFILE=${RCWFILE%\"*}
+	local DESTRCW="rcw_1300.bin"
+
 	# build the itb image
 	cp board/nxp/ls1028ardb/kernel-ls1028a-rdb.its ${2}/
 	cp output/images/fsl-ls1028a-rdb.dtb ${2}/
@@ -18,6 +22,7 @@ main()
 	cd ${2}/
 	mkimage -f kernel-ls1028a-rdb.its kernel-ls1028a-rdb.itb
 	cd ${3}
+	cp ${BINARIES_DIR}/${RCWFILE} ${BINARIES_DIR}/${DESTRCW}
 	cp ${2}/kernel-ls1028a-rdb.itb ${BINARIES_DIR}/
 	rm ${2}/fsl-image-core-ls1028ardb.ext2.gz
 	rm ${2}/fsl-ls1028a-rdb.dtb

Makefile
@@ -467,6 +467,7 @@ export HOST_DIR
 export BINARIES_DIR
 export BASE_DIR
 export BR2_ROOTFS_PARTITION_SIZE
+export BR2_PACKAGE_RCW_BIN
 
 ################################################################################
 #

board/nxp/ls1028ardb/genimage.cfg.template
@@ -22,7 +22,7 @@ image sdcard.img {
 
   partition rcw {
     in-partition-table = "no"
-    image = "rcw_1300_sd.bin"
+    image = "rcw_1300.bin"
     offset = 4096
   }
 

commit de4e67e236cbb0253dab41e214538cd20642b27c

openil-package/openil-rcw: Create new RCW package for LS1028A-RDB
The LS1028A-RDB support is so hacky in order to be able to generate a
eMMC/SD either-or boot source combination, and along with the fact that
OpenIL has changed the download source to the LS1028A BSP repository,
that it's better to just keep the upstream Buildroot rcw package intact
and simply fork the custom rcw into an OpenIL-specific package.

Fixes: 675d23bd6bf6 ("ls1028ardb: unify the RCW file for SD boot and eMMC boot")
Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

board/nxp/ls1028ardb/post-image-64b-baremetal.sh
@@ -3,6 +3,7 @@
 # $2 linux building directory
 # $3 buildroot top directory
 # $4 u-boot building directory
+# $5 BR2_PACKAGE_OPENIL_RCW_BIN
 
 main()
 {
@@ -10,7 +11,7 @@ main()
 	echo ${3}
 	echo ${4}
 
-	local RCWFILE=${BR2_PACKAGE_RCW_BIN##*/}
+	local RCWFILE=${5##*/}
 	RCWFILE=${RCWFILE%\"*}
 	local DESTRCW="rcw_1300.bin"
 

board/nxp/ls1028ardb/post-image-64b-ubuntu.sh
@@ -3,6 +3,7 @@
 # $2 linux building directory
 # $3 buildroot top directory
 # $4 u-boot building directory
+# $5 BR2_PACKAGE_OPENIL_RCW_BIN
 
 main()
 {
@@ -10,7 +11,7 @@ main()
 	echo ${3}
 	echo ${4}
 
-	local RCWFILE=${BR2_PACKAGE_RCW_BIN##*/}
+	local RCWFILE=${5##*/}
 	RCWFILE=${RCWFILE%\"*}
 	local DESTRCW="rcw_1300.bin"
 

board/nxp/ls1028ardb/post-image-64b.sh
@@ -3,6 +3,7 @@
 # $2 linux building directory
 # $3 buildroot top directory
 # $4 u-boot building directory
+# $5 BR2_PACKAGE_OPENIL_RCW_BIN
 
 main()
 {
@@ -10,7 +11,7 @@ main()
 	echo ${3}
 	echo ${4}
 
-	local RCWFILE=${BR2_PACKAGE_RCW_BIN##*/}
+	local RCWFILE=${5##*/}
 	RCWFILE=${RCWFILE%\"*}
 	local DESTRCW="rcw_1300.bin"
 

configs/nxp_ls1028ardb-64b-emmc_defconfig
@@ -57,7 +57,7 @@ BR2_PACKAGE_HOST_DOSFSTOOLS=y
 BR2_PACKAGE_HOST_GENIMAGE=y
 BR2_PACKAGE_HOST_MTOOLS=y
 BR2_ROOTFS_POST_IMAGE_SCRIPT="board/nxp/ls1028ardb/post-image-64b.sh"
-BR2_ROOTFS_POST_SCRIPT_ARGS="$(LINUX_DIR) $(TOPDIR) $(UBOOT_DIR)"
+BR2_ROOTFS_POST_SCRIPT_ARGS="$(LINUX_DIR) $(TOPDIR) $(UBOOT_DIR) $(BR2_PACKAGE_OPENIL_RCW_BIN)"
 
 # busybox setting
 BR2_PACKAGE_BUSYBOX_CONFIG_FRAGMENT_FILES="board/nxp/common/openil-busybox.config"
@@ -67,8 +67,8 @@ BR2_PACKAGE_BUSYBOX_SHOW_OTHERS=y
 BR2_ROOTFS_OVERLAY="board/nxp/common/ota_overlay board/nxp/common/busybox_init_overlay"
 
 # packages for rcw
-BR2_PACKAGE_RCW=y
-BR2_PACKAGE_RCW_BIN="ls1028ardb/R_SQPP_85bb/rcw_1300_emmc.bin"
+BR2_PACKAGE_OPENIL_RCW=y
+BR2_PACKAGE_OPENIL_RCW_BIN="ls1028ardb/R_SQPP_85bb/rcw_1300_emmc.bin"
 
 # ppa firmware
 BR2_PACKAGE_PPA=y

configs/nxp_ls1028ardb-64b_defconfig
@@ -57,7 +57,7 @@ BR2_PACKAGE_HOST_DOSFSTOOLS=y
 BR2_PACKAGE_HOST_GENIMAGE=y
 BR2_PACKAGE_HOST_MTOOLS=y
 BR2_ROOTFS_POST_IMAGE_SCRIPT="board/nxp/ls1028ardb/post-image-64b.sh"
-BR2_ROOTFS_POST_SCRIPT_ARGS="$(LINUX_DIR) $(TOPDIR) $(UBOOT_DIR)"
+BR2_ROOTFS_POST_SCRIPT_ARGS="$(LINUX_DIR) $(TOPDIR) $(UBOOT_DIR) $(BR2_PACKAGE_OPENIL_RCW_BIN)"
 
 # busybox setting
 BR2_PACKAGE_BUSYBOX_CONFIG_FRAGMENT_FILES="board/nxp/common/openil-busybox.config"
@@ -67,8 +67,8 @@ BR2_PACKAGE_BUSYBOX_SHOW_OTHERS=y
 BR2_ROOTFS_OVERLAY="board/nxp/common/ota_overlay board/nxp/common/busybox_init_overlay"
 
 # packages for rcw
-BR2_PACKAGE_RCW=y
-BR2_PACKAGE_RCW_BIN="ls1028ardb/R_SQPP_85bb/rcw_1300_sd.bin"
+BR2_PACKAGE_OPENIL_RCW=y
+BR2_PACKAGE_OPENIL_RCW_BIN="ls1028ardb/R_SQPP_85bb/rcw_1300_sd.bin"
 
 # ppa firmware
 BR2_PACKAGE_PPA=y

configs/nxp_ls1028ardb-64b_ubuntu_defconfig
@@ -65,7 +65,7 @@ BR2_PACKAGE_HOST_GENIMAGE=y
 BR2_PACKAGE_HOST_MTOOLS=y
 BR2_ROOTFS_POST_BUILD_SCRIPT="board/nxp/common/post-build-ubuntu-base-16.04.sh"
 BR2_ROOTFS_POST_IMAGE_SCRIPT="board/nxp/ls1028ardb/post-image-64b-ubuntu.sh"
-BR2_ROOTFS_POST_SCRIPT_ARGS="$(LINUX_DIR) $(TOPDIR) $(UBOOT_DIR)"
+BR2_ROOTFS_POST_SCRIPT_ARGS="$(LINUX_DIR) $(TOPDIR) $(UBOOT_DIR) $(BR2_PACKAGE_OPENIL_RCW_BIN)"
 BR2_ROOTFS_POST_CFG_UDEV="board/nxp/ls1028ardb/post-cfg-udev.sh"
 
 # Add ota overlay
@@ -86,8 +86,8 @@ BR2_PACKAGE_DHCP_CLIENT=y
 BR2_PACKAGE_PSMISC=y
 
 # packages for rcw
-BR2_PACKAGE_RCW=y
-BR2_PACKAGE_RCW_BIN="ls1028ardb/R_SQPP_85bb/rcw_1300_sd.bin"
+BR2_PACKAGE_OPENIL_RCW=y
+BR2_PACKAGE_OPENIL_RCW_BIN="ls1028ardb/R_SQPP_85bb/rcw_1300_sd.bin"
 
 # ppa firmware
 BR2_PACKAGE_PPA=y

configs/nxp_ls1028ardb_baremetal-64b_defconfig
@@ -57,7 +57,7 @@ BR2_PACKAGE_HOST_DOSFSTOOLS=y
 BR2_PACKAGE_HOST_GENIMAGE=y
 BR2_PACKAGE_HOST_MTOOLS=y
 BR2_ROOTFS_POST_IMAGE_SCRIPT="board/nxp/ls1028ardb/post-image-64b-baremetal.sh"
-BR2_ROOTFS_POST_SCRIPT_ARGS="$(LINUX_DIR) $(TOPDIR) $(UBOOT_DIR)"
+BR2_ROOTFS_POST_SCRIPT_ARGS="$(LINUX_DIR) $(TOPDIR) $(UBOOT_DIR) $(BR2_PACKAGE_OPENIL_RCW_BIN)"
 
 # busybox setting
 BR2_PACKAGE_BUSYBOX_CONFIG_FRAGMENT_FILES="board/nxp/common/openil-busybox.config"
@@ -67,8 +67,8 @@ BR2_PACKAGE_BUSYBOX_SHOW_OTHERS=y
 BR2_ROOTFS_OVERLAY="board/nxp/common/ota_overlay board/nxp/common/busybox_init_overlay"
 
 # packages for rcw
-BR2_PACKAGE_RCW=y
-BR2_PACKAGE_RCW_BIN="ls1028ardb/R_SQPP_85bb/rcw_1300_sd.bin"
+BR2_PACKAGE_OPENIL_RCW=y
+BR2_PACKAGE_OPENIL_RCW_BIN="ls1028ardb/R_SQPP_85bb/rcw_1300_sd.bin"
 
 # ppa firmware
 BR2_PACKAGE_PPA=y

openil-package/Config.in
@@ -28,6 +28,9 @@ endmenu
 menu "Fman ucode"
 	source "openil-package/fmucode/Config.in"
 endmenu
+menu "rcw image"
+	source "openil-package/openil-rcw/Config.in"
+endmenu
 menu "ppfe firmware"
 	source "openil-package/ppfe/Config.in"
 endmenu

openil-package/openil-rcw/Config.in
@@ -1,9 +1,9 @@
-config BR2_PACKAGE_RCW
+config BR2_PACKAGE_OPENIL_RCW
 	bool "rcw image"
 	help
 	The reset configuration word for NXP Layerscape platforms
 
-if BR2_PACKAGE_RCW
-config BR2_PACKAGE_RCW_BIN
+if BR2_PACKAGE_OPENIL_RCW
+config BR2_PACKAGE_OPENIL_RCW_BIN
 	string "Custom RCW"
 endif

openil-package/openil-rcw/Config.in.host
@@ -1,0 +1,12 @@
+config BR2_PACKAGE_HOST_OPENIL_RCW
+	bool "host rcw"
+	help
+	  This package provides an reset configuration word(RCW)
+	  compiler to build NXP QoriQ/LS PBL/RCW binary(s).
+
+	  The package installs example configurations and scripts
+	  for each devkit supported in the current release see
+	  $(HOST_DIR)/share/rcw. Either a SDK or post scripts can
+	  then use this toolset and examples.
+
+	  https://source.codeaurora.org/external/qoriq/qoriq-components/rcw/

openil-package/openil-rcw/openil-rcw.mk
@@ -1,0 +1,36 @@
+################################################################################
+#
+# openil-rcw image
+#
+################################################################################
+
+OPENIL_RCW_VERSION = ls1028a-early-access
+OPENIL_RCW_SITE = https://source.codeaurora.org/external/qoriq/qoriq-components/rcw
+OPENIL_RCW_SITE_METHOD = git
+OPENIL_RCW_LICENSE = BSD License
+OPENIL_RCW_LICENSE_FILES = LICENSE
+
+OPENIL_RCW_BIN = $(call qstrip,$(BR2_PACKAGE_OPENIL_RCW_BIN))
+OPENIL_RCW_PLATFORM = $(firstword $(subst /, ,$(OPENIL_RCW_BIN)))
+
+FIND = ls1028ardb
+ifeq ($(findstring $(FIND), $(BR2_TARGET_UBOOT_BOARDNAME)), $(FIND))
+define OPENIL_RCW_CONFIGURE_CMDS
+	$(APPLY_PATCHES) $(@D) $(OPENIL_RCW_PKGDIR) \
+		0001-ls1028ardb-Enable-IIC5_PMUX-for-GPIO-function.patch.conditional; \
+	$(APPLY_PATCHES) $(@D) $(OPENIL_RCW_PKGDIR) \
+		0002-ls1028ardb-Enable-CLK_OUT_PMUX-for-GPIO-function.patch.conditional;
+endef
+endif
+
+define OPENIL_RCW_BUILD_CMDS
+	cd $(@D)/$(OPENIL_RCW_PLATFORM) && $(MAKE); \
+	if [ $(OPENIL_RCW_PLATFORM) = ls1046ardb ] || [ $(OPENIL_RCW_PLATFORM) = ls1012ardb ]; then \
+		tclsh board/nxp/common/byte_swap.tcl $(@D)/$(OPENIL_RCW_BIN) $(@D)/$(OPENIL_RCW_BIN) 8; \
+		cp -f $(@D)/$(OPENIL_RCW_BIN).swapped $(BINARIES_DIR); \
+	else \
+		cp -f $(@D)/$(OPENIL_RCW_BIN) $(BINARIES_DIR); \
+	fi
+endef
+
+$(eval $(generic-package))