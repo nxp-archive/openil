af5b9ea248a698c9a4fd1dac9a90096dec3ebb83
Jerry Huang <jerry.huang@nxp.com>
2019-10-25 14:54:37 +0800

    OpenIL/LS1028ARDB: Workaround for ENETC PHY negotiation issue
    With Linux Kernel 4.19 in LSDK-19.09, After enabling AT803X PHY,
    the network link status is not correct:
            Speed: 1000Mb/s
            Duplex: Full
            Port: MII
            PHYAD: 2
            Transceiver: internal
            Auto-negotiation: on
            Link detected: no
    
    Two ways to make network works fine:
    1> Disable PHY AT803X, use the general phy
    2> To change the priority of printk after kernel bootup:
    echo '7 4 1 7' > /proc/sys/kernel/printk
    
    We use the second way as one workaround to make network work fine.
    
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

--- board/nxp/ls1028ardb/post-custom-skeleton-ubuntu-base-16.04.sh
+++ board/nxp/ls1028ardb/post-custom-skeleton-ubuntu-base-16.04.sh
@@ -14,6 +14,9 @@ main()
 	# enable the root user login
 	sed -i 's/root:\*:/root::/' etc/shadow
 
+	# workaround for ls1028ardb enetc phy issue in Linux kernel 4.19
+	sed -i "/exit 0/i\echo '7 4 1 7' > /proc/sys/kernel/printk" etc/rc.local
+
 	# create the link for mount and umount for the systemd
 	ln -s /bin/mount usr/bin/mount
 	ln -s /bin/umount usr/bin/umount
