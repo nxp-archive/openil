eee6fba2a360f37827024c64e9cf8efcfaec935c
Jerry Huang <jerry.huang@nxp.com>
2019-12-20 16:31:45 +0800

    OpenIL/LS1012ARDB: Enable QSPI boot with 32bit Linux Kernel and TFA support
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

--- board/nxp/ls1012ardb/post-image.sh
+++ board/nxp/ls1012ardb/post-image.sh
@@ -14,31 +14,28 @@ main()
 	# build the itb image
 	cp board/nxp/ls1012ardb/kernel-ls1012a-rdb-aarch32.its ${2}/
 	cp output/images/fsl-ls1012a-rdb.dtb ${2}/
-	cp ${BINARIES_DIR}/rootfs.ext2.gz ${2}/fsl-image-core-ls1012ardb-32b.ext2.gz
+	cp ${BINARIES_DIR}/rootfs.cpio.gz ${2}/fsl-image-core-ls1012ardb-32b.cpio.gz
 	cd ${2}/
-	mkimage -f kernel-ls1012a-rdb-aarch32.its kernel-ls1012a-rdb-aarch32.itb
+	/usr/bin/mkimage -f kernel-ls1012a-rdb-aarch32.its kernel-ls1012a-rdb-aarch32.itb
 	cd ${3}
 	cp ${2}/kernel-ls1012a-rdb-aarch32.itb ${BINARIES_DIR}/
-	rm ${2}/fsl-image-core-ls1012ardb-32b.ext2.gz
+	rm ${2}/fsl-image-core-ls1012ardb-32b.cpio.gz
 	rm ${2}/fsl-ls1012a-rdb.dtb
 	rm ${2}/kernel-ls1012a-rdb-aarch32.itb
 	rm ${2}/kernel-ls1012a-rdb-aarch32.its
 	ln -s ${BINARIES_DIR}/kernel-ls1012a-rdb-aarch32.itb ${BINARIES_DIR}/kernel-ls1012a-rdb.itb
 
-	# build the ramdisk rootfs
-	mkimage -A arm -T ramdisk -C gzip -d ${BINARIES_DIR}/rootfs.ext2.gz ${BINARIES_DIR}/rootfs.ext2.gz.uboot
-
 	# define board name in version.json for ota feature
 	local BOARDNAME="ls1012ardb-32b"
 	sed -e "s/%PLATFORM%/${BOARDNAME}/" board/nxp/common/version.json > ${BINARIES_DIR}/version.json
 
-	# build the SDcard image
+	# build the QSPI image
 	local FILES=""kernel-ls1012a-rdb-aarch32.itb", "version.json""
 	local GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
 	local GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 	sed -e "s/%FILES%/${FILES}/" \
-		board/nxp/ls1012ardb/genimage.cfg.template > ${GENIMAGE_CFG}
+		board/nxp/ls1012ardb/genimage.32b.cfg.template > ${GENIMAGE_CFG}
 
 	rm -rf "${GENIMAGE_TMP}"
 
@@ -50,8 +47,6 @@ main()
 		--config "${GENIMAGE_CFG}"
 
 	rm -f ${GENIMAGE_CFG}
-
-	mv ${BINARIES_DIR}/sdcard.img ${BINARIES_DIR}/qspi.img
 
 	exit $?
 }
