c016f5cd64052aa952a2bc76265abb7047e590c3
Jerry Huang <jerry.huang@nxp.com>
2019-12-20 16:29:01 +0800

    OpenIL/LS1012ARDB: Enable QSPI boot with 64bit Linux Kernel and TFA support
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

--- board/nxp/ls1012ardb/post-image-64b.sh
+++ board/nxp/ls1012ardb/post-image-64b.sh
@@ -13,24 +13,21 @@ main()
 	# build the itb image
 	cp board/nxp/ls1012ardb/kernel-ls1012a-rdb.its ${2}/
 	cp output/images/fsl-ls1012a-rdb.dtb ${2}/
-	cp ${BINARIES_DIR}/rootfs.ext2.gz ${2}/fsl-image-core-ls1012ardb.rootfs.ext2.gz
+	cp ${BINARIES_DIR}/rootfs.cpio.gz ${2}/fsl-image-core-ls1012ardb.rootfs.cpio.gz
 	cd ${2}/
-	mkimage -f kernel-ls1012a-rdb.its kernel-ls1012a-rdb.itb
+	/usr/bin/mkimage -f kernel-ls1012a-rdb.its kernel-ls1012a-rdb.itb
 	cd ${3}
 	cp ${2}/kernel-ls1012a-rdb.itb ${BINARIES_DIR}/
-	rm ${2}/fsl-image-core-ls1012ardb.rootfs.ext2.gz
+	rm ${2}/fsl-image-core-ls1012ardb.rootfs.cpio.gz
 	rm ${2}/fsl-ls1012a-rdb.dtb
 	rm ${2}/kernel-ls1012a-rdb.itb
 	rm ${2}/kernel-ls1012a-rdb.its
-
-	# build the ramdisk rootfs
-	mkimage -A arm -T ramdisk -C gzip -d ${BINARIES_DIR}/rootfs.ext2.gz ${BINARIES_DIR}/rootfs.ext2.gz.uboot
 
 	# define board name in version.json for ota feature
 	local BOARDNAME="ls1012ardb-64b"
 	sed -e "s/%PLATFORM%/${BOARDNAME}/" board/nxp/common/version.json > ${BINARIES_DIR}/version.json
 
-	# build the SDcard image
+	# build the QSPI image
 	local FILES=""kernel-ls1012a-rdb.itb", "version.json""
 	local GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
 	local GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
@@ -49,7 +46,20 @@ main()
 
 	rm -f ${GENIMAGE_CFG}
 
-#	mv ${BINARIES_DIR}/sdcard.img ${BINARIES_DIR}/qspi.img
+	# Build the SD card image
+	sed -e "s/%FILES%/${FILES}/" \
+		board/nxp/ls1012ardb/genimage.sd.cfg.template > ${GENIMAGE_CFG}
+
+	rm -rf "${GENIMAGE_TMP}"
+
+	genimage \
+		--rootpath "${TARGET_DIR}" \
+		--tmppath "${GENIMAGE_TMP}" \
+		--inputpath "${BINARIES_DIR}" \
+		--outputpath "${BINARIES_DIR}" \
+		--config "${GENIMAGE_CFG}"
+
+	rm -f ${GENIMAGE_CFG}
 
 	exit $?
 }
