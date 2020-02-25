49bb777e78c4d47ef047e6635e6f4316b51b928f
Liu Gang <Gang.Liu@nxp.com>
2020-02-25 17:49:48 +0800

    package/nxp/qoriq-bm-uboot: new package
    For some multiple cores silicon and AMP system, one or more
    cores can run baremetal software firmware in order to get
    better performance.
    
    The baremetal package is created based on Uboot project, and
    can be deployed on one or more cores.
    
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

---
+++ package/nxp/qoriq-bm-uboot/qoriq-bm-uboot.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# qoriq-bm-uboot
+#
+################################################################################
+
+QORIQ_BM_UBOOT_VERSION = $(call qstrip,$(BR2_PACKAGE_QORIQ_BM_UBOOT_VERSION))
+QORIQ_BM_UBOOT_SITE = https://github.com/openil/u-boot.git
+QORIQ_BM_UBOOT_SITE_METHOD = git
+QORIQ_BM_UBOOT_LICENSE = GPL-2.0+
+QORIQ_BM_UBOOT_LICENSE_FILES = README
+QORIQ_BM_UBOOT_DEPENDENCIES += host-dtc
+
+QORIQ_BM_UBOOT_MAKE_OPTS = \
+	CC="$(TARGET_CC)" \
+	CROSS_COMPILE="$(TARGET_CROSS)" \
+
+QORIQ_BM_UBOOT_DEFCONFIG = $(call qstrip,$(BR2_PACKAGE_QORIQ_BM_UBOOT_DEFCONFIG))
+
+define QORIQ_BM_UBOOT_BUILD_CMDS
+	cd $(@D)/ && $(MAKE) distclean
+	cd $(@D)/ && $(MAKE) $(QORIQ_BM_UBOOT_DEFCONFIG)
+	$(TARGET_MAKE_ENV) $(MAKE) $(QORIQ_BM_UBOOT_MAKE_OPTS)-C $(@D)
+endef
+
+define QORIQ_BM_UBOOT_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/u-boot.bin $(BINARIES_DIR)/bm-u-boot.bin
+endef
+
+$(eval $(generic-package))
