448e60d6bd474b16fbe31fd2193e0113d9950c65
Yangbo Lu <yangbo.lu@nxp.com>
2020-02-27 21:30:57 +0800

    package/sja1105-ptp: update to commit eee6d0f
    Update the package to latest commit eee6d0f. The sja1105-ptp
    application could be dropped, and use just the shell script
    configuring 2-step E2E TC instead. This package will be dropped
    once sja1105 DSA driver is supported in Linux, since linuxptp
    could be used to support TC.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- package/sja1105-ptp/sja1105-ptp.mk
+++ package/sja1105-ptp/sja1105-ptp.mk
@@ -4,31 +4,17 @@
 #
 ################################################################################
 
-SJA1105_PTP_VERSION = OpenIL-linuxptp-201712
+SJA1105_PTP_VERSION = eee6d0fb995dddf27acde2addfb1fe89b71ab82d
 SJA1105_PTP_SITE = https://github.com/openil/linuxptp.git
 SJA1105_PTP_SITE_METHOD = git
 SJA1105_PTP_LICENSE = GPL2
 SJA1105_PTP_LICENSE_FILES = COPYING
-ifeq ($(BR2_PACKAGE_SJA1105_TOOL),y)
-SJA1105_PTP_DEPENDENCIES = sja1105-tool
-SJA1105_OUTPUT=$(STAGING_DIR)/usr
-endif
-
-SJA1105_PTP_MAKE_OPTS = \
-	CC="$(TARGET_CC)" \
-	CROSS_COMPILE="$(TARGET_CROSS)" \
-	SJA1105_ROOTDIR="$(SJA1105_OUTPUT)" \
-	SJA1105_PTP_TC=y \
 
 define SJA1105_PTP_BUILD_CMDS
-	export PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)"; \
-	export KBUILD_OUTPUT="$(STAGING_DIR)"; \
-	$(TARGET_MAKE_ENV) $(MAKE1) $(SJA1105_PTP_MAKE_OPTS) -C $(@D)
 endef
 
 define SJA1105_PTP_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/sja1105-ptp $(TARGET_DIR)/usr/sbin/sja1105-ptp
-	cp -dpfr $(@D)/sja1105-ptp-tc.sh $(TARGET_DIR)/usr/sbin/sja1105-ptp-tc.sh
+	cp -dpfr $(@D)/sja1105-ptp-free-tc.sh $(TARGET_DIR)/usr/sbin/sja1105-ptp-free-tc.sh
 endef
 
 $(eval $(generic-package))
