a4d83ff2bb960be1e334408c140eafdf6e849714
Liu Gang <Gang.Liu@nxp.com>
2020-03-04 20:08:11 +0800

    board/nxp/ls1012ardb: new board
    Support qoriq ls1012ardb
    configs/nxp_ls1012ardb-64b_defconfig: boot from QSPI flash
    
    Signed-off-by: Liu Gang <Gang.Liu@nxp.com>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

---
+++ board/nxp/ls1012ardb/post-image.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# args from BR2_ROOTFS_POST_SCRIPT_ARGS
+# $2 linux building directory
+# $3 buildroot top directory
+# $4 uboot building directory
+
+main()
+{
+	# build the itb image
+	cp board/nxp/ls1012ardb/kernel.its ${BINARIES_DIR}/kernel.its
+	cd ${BINARIES_DIR}/
+	/usr/bin/mkimage -f kernel.its kernel.itb
+	rm kernel.its
+
+	cd ${3}
+
+	# build the QSPI image
+	local GENIMAGE_CFG="$(mktemp --suffix genimage.cfg)"
+	local GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+	sed -e "s/%FILES%/${FILES}/" \
+		board/nxp/ls1012ardb/genimage.cfg.template > ${GENIMAGE_CFG}
+
+	rm -rf "${GENIMAGE_TMP}"
+
+	genimage \
+		--rootpath "${TARGET_DIR}" \
+		--tmppath "${GENIMAGE_TMP}" \
+		--inputpath "${BINARIES_DIR}" \
+		--outputpath "${BINARIES_DIR}" \
+		--config "${GENIMAGE_CFG}"
+
+	rm -f ${GENIMAGE_CFG}
+
+	exit $?
+}
+
+main $@
