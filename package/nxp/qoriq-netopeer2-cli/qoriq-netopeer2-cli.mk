36a2a35f4938cf548a639b4d728a84df502e14fd
Xiaolin He <xiaolin.he@nxp.com>
2020-03-12 16:08:07 +0800

    package/nxp/qoriq-netopeer2-cli: new package
    Signed-off-by: Xiaolin He <xiaolin.he@nxp.com>
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

---
+++ package/nxp/qoriq-netopeer2-cli/qoriq-netopeer2-cli.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# qoriq-netopeer2-cli
+#
+################################################################################
+
+QORIQ_NETOPEER2_CLI_VERSION = 1.1.1
+QORIQ_NETOPEER2_CLI_SITE = $(call github,CESNET,Netopeer2,v$(QORIQ_NETOPEER2_CLI_VERSION))
+QORIQ_NETOPEER2_CLI_SUBDIR = cli
+QORIQ_NETOPEER2_CLI_INSTALL_STAGING = NO
+QORIQ_NETOPEER2_CLI_LICENSE = BSD-3c
+QORIQ_NETOPEER2_CLI_LICENSE_FILES = LICENSE
+QORIQ_NETOPEER2_CLI_DEPENDENCIES = libyang libnetconf2
+QORIQ_NETOPEER2_CLI_IMAKE_ENV = LD_LIBRARY_PATH+=$(HOST_DIR)/usr/lib:$(HOST_DIR)/lib
+
+$(eval $(cmake-package))
