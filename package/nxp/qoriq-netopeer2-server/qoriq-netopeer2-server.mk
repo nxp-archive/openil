d90cfceb4566232fbedc0471464efeec5a4e5edb
Xiaolin He <xiaolin.he@nxp.com>
2020-03-12 16:05:45 +0800

    package/nxp/qoriq-netopeer2-server: new package
    Signed-off-by: Xiaolin He <xiaolin.he@nxp.com>
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

---
+++ package/nxp/qoriq-netopeer2-server/qoriq-netopeer2-server.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# qoriq-netopeer2-server
+#
+################################################################################
+
+QORIQ_NETOPEER2_SERVER_VERSION = 1.1.1
+QORIQ_NETOPEER2_SERVER_SITE = $(call github,CESNET,Netopeer2,v$(QORIQ_NETOPEER2_SERVER_VERSION))
+QORIQ_NETOPEER2_SERVER_SUBDIR = server
+QORIQ_NETOPEER2_SERVER_INSTALL_STAGING = NO
+QORIQ_NETOPEER2_SERVER_LICENSE = BSD-3c
+QORIQ_NETOPEER2_SERVER_LICENSE_FILES = LICENSE
+QORIQ_NETOPEER2_SERVER_DEPENDENCIES = libnetconf2 sysrepo libyang
+
+define QORIQ_NETOPEER2_SERVER_INSTALL_DAEMON_SCRIPT
+	$(INSTALL) -D -m 0751 package/nxp/qoriq-netopeer2-server/S91netopeer2-server \
+		$(TARGET_DIR)/etc/init.d/
+endef
+
+QORIQ_NETOPEER2_SERVER_POST_INSTALL_TARGET_HOOKS = QORIQ_NETOPEER2_SERVER_INSTALL_DAEMON_SCRIPT
+
+# prevent an attempted chown to root:root
+QORIQ_NETOPEER2_SERVER_CONF_OPTS += -DSYSREPOCTL_ROOT_PERMS="-p 666"
+QORIQ_NETOPEER2_SERVER_MAKE_ENV = LD_LIBRARY_PATH+=$(HOST_DIR)/usr/lib:$(HOST_DIR)/lib
+# the .pc file is for the target, and therefore not consulted during the build
+QORIQ_NETOPEER2_SERVER_CONF_OPTS += -DKEYSTORED_KEYS_DIR=/etc/keystored/keys
+
+$(eval $(cmake-package))
