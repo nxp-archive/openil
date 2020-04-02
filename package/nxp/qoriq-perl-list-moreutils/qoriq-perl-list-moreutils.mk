981bf3ee2818cdd9919e71ea78d5475f8ac0ff68
Jerry Huang <jerry.huang@nxp.com>
2020-04-02 22:00:01 +0800

    package/nxp/qoriq-perl-list-moreutils: add version option BR2_PACKAGE_QORIQ_PERL_LIST_MOREUTILS_VERSION
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

--- package/nxp/qoriq-perl-list-moreutils/qoriq-perl-list-moreutils.mk
+++ package/nxp/qoriq-perl-list-moreutils/qoriq-perl-list-moreutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QORIQ_PERL_LIST_MOREUTILS_VERSION = 0.426
+QORIQ_PERL_LIST_MOREUTILS_VERSION = $(call qstrip,$(BR2_PACKAGE_QORIQ_PERL_LIST_MOREUTILS_VERSION))
 QORIQ_PERL_LIST_MOREUTILS_SOURCE = List-MoreUtils-$(QORIQ_PERL_LIST_MOREUTILS_VERSION).tar.gz
 QORIQ_PERL_LIST_MOREUTILS_SITE = http://search.cpan.org/CPAN/authors/id/R/RE/REHSACK
 QORIQ_PERL_LIST_MOREUTILS_LICENSE = Apache
