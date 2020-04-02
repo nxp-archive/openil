8d7b40af52e6b9c1cf4408f85073d1971aaf32cc
Jerry Huang <jerry.huang@nxp.com>
2020-04-02 22:01:37 +0800

    package/nxp/qoriq-python-wand: add version option
    BR2_PACKAGE_QORIQ_PYTHON_WAND_VERSION
    
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

--- package/nxp/qoriq-python-wand/qoriq-python-wand.mk
+++ package/nxp/qoriq-python-wand/qoriq-python-wand.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QORIQ_PYTHON_WAND_VERSION = 0.4.4
+QORIQ_PYTHON_WAND_VERSION = $(call qstrip,$(BR2_PACKAGE_QORIQ_PYTHON_WAND_VERSION))
 QORIQ_PYTHON_WAND_SITE = https://github.com/emcconville/wand.git
 QORIQ_PYTHON_WAND_SITE_METHOD = git
 QORIQ_PYTHON_WAND_LICENSE = MIT
