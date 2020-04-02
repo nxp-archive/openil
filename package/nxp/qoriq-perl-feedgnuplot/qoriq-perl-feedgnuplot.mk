5196454c8c29e2caf6298ea5345ca847e3e414d2
Jerry Huang <jerry.huang@nxp.com>
2020-04-02 21:58:54 +0800

    package/nxp/qoriq-perl-feedgnuplot: add version option BR2_PACKAGE_QORIQ_PERL_FEEDGNUPLOT_VERSION
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

--- package/nxp/qoriq-perl-feedgnuplot/qoriq-perl-feedgnuplot.mk
+++ package/nxp/qoriq-perl-feedgnuplot/qoriq-perl-feedgnuplot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QORIQ_PERL_FEEDGNUPLOT_VERSION = v1.45
+QORIQ_PERL_FEEDGNUPLOT_VERSION = $(call qstrip,$(BR2_PACKAGE_QORIQ_PERL_FEEDGNUPLOT_VERSION))
 QORIQ_PERL_FEEDGNUPLOT_SITE = https://github.com/dkogan/feedgnuplot.git
 QORIQ_PERL_FEEDGNUPLOT_SITE_METHOD = git
 QORIQ_PERL_FEEDGNUPLOT_DEPENDENCIES = gnuplot qoriq-perl-list-moreutils qoriq-perl-exporter-tiny
