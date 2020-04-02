8118e39d7479ded39a7fbc4bd7d80cc365613a47
Jerry Huang <jerry.huang@nxp.com>
2020-04-02 21:58:17 +0800

    package/nxp/qoriq-perl-exporter-tiny: add version option BR2_PACKAGE_QORIQ_PERL_EXPORTER_TINY_VERSION
    Signed-off-by: Jerry Huang <jerry.huang@nxp.com>

--- package/nxp/qoriq-perl-exporter-tiny/qoriq-perl-exporter-tiny.mk
+++ package/nxp/qoriq-perl-exporter-tiny/qoriq-perl-exporter-tiny.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QORIQ_PERL_EXPORTER_TINY_VERSION = 1.000000
+QORIQ_PERL_EXPORTER_TINY_VERSION = $(call qstrip,$(BR2_PACKAGE_QORIQ_PERL_EXPORTER_TINY_VERSION))
 QORIQ_PERL_EXPORTER_TINY_SOURCE = Exporter-Tiny-$(QORIQ_PERL_EXPORTER_TINY_VERSION).tar.gz
 QORIQ_PERL_EXPORTER_TINY_SITE = http://search.cpan.org/CPAN/authors/id/T/TO/TOBYINK
 QORIQ_PERL_EXPORTER_TINY_LICENSE = Artistic or GPL-1.0+
