c00762210e7603d37070e38395a11787069e3373
Changming Huang <jerry.huang@nxp.com>
2020-06-19 13:16:05 +0800

    package/imx-mkimage: bump to version rel_imx_5.4.24_2.1.0
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

--- package/imx-mkimage/imx-mkimage.mk
+++ package/imx-mkimage/imx-mkimage.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-IMX_MKIMAGE_VERSION = rel_imx_4.14.98_2.0.0_ga
+IMX_MKIMAGE_VERSION = rel_imx_5.4.24_2.1.0
 IMX_MKIMAGE_SITE = https://source.codeaurora.org/external/imx/imx-mkimage
 IMX_MKIMAGE_SITE_METHOD = git
 IMX_MKIMAGE_LICENSE = GPL-2.0+
 IMX_MKIMAGE_LICENSE_FILES = COPYING
 HOST_IMX_MKIMAGE_DEPENDENCIES = host-zlib
 
-ifeq ($(BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8M)$(BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8MM),y)
+ifeq ($(BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8M)$(BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8MM)$(BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8MP),y)
 # i.MX8M needs a different binary
 define HOST_IMX_MKIMAGE_BUILD_CMDS
 	$(HOST_MAKE_ENV) $(MAKE) $(HOST_CONFIGURE_OPTS) \
@@ -21,6 +21,7 @@ endef
 define HOST_IMX_MKIMAGE_INSTALL_CMDS
 	$(INSTALL) -D -m 755 $(@D)/iMX8M/mkimage_imx8 $(HOST_DIR)/bin/mkimage_imx8
 	$(INSTALL) -D -m 755 $(@D)/iMX8M/mkimage_fit_atf.sh $(HOST_DIR)/bin/mkimage_fit_atf.sh
+	$(INSTALL) -D -m 755 $(@D)/scripts/pad_image.sh $(HOST_DIR)/bin/pad_image.sh
 endef
 else
 # i.MX8 and i.MX8X
