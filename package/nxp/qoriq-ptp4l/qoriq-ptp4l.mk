nxp-archive/openil

commit 24f79f6ddb054a86aa1de107ac1244ce7361fa3e

package/nxp/qoriq-ptp4l: use compiled kernel headers
For phc2sys to use the PTP_SYS_OFFSET_EXTENDED ioctl without depending
on a particular set of linux-headers brought by a toolchain (external or
built by buildroot), just specify the path to the compiled Linux kernel.

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
Signed-off-by: Changming Huang <jerry.huang@nxp.com>

package/nxp/qoriq-ptp4l/qoriq-ptp4l.mk
@@ -17,13 +17,13 @@ QORIQ_PTP4L_MAKE_OPTS = \
 
 define QORIQ_PTP4L_BUILD_CMDS
 	export PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)"; \
-	export KBUILD_OUTPUT="$(STAGING_DIR)"; \
+	export KBUILD_OUTPUT="$(BUILD_DIR)/linux-$(BR2_LINUX_KERNEL_CUSTOM_REPO_VERSION)"; \
 	$(TARGET_MAKE_ENV) $(MAKE1) $(QORIQ_PTP4L_MAKE_OPTS) -C $(@D)
 endef
 
 define QORIQ_PTP4L_INSTALL_TARGET_CMDS
 	export PKG_CONFIG_SYSROOT_DIR="$(STAGING_DIR)"; \
-	export KBUILD_OUTPUT="$(STAGING_DIR)"; \
+	export KBUILD_OUTPUT="$(BUILD_DIR)/linux-$(BR2_LINUX_KERNEL_CUSTOM_REPO_VERSION)"; \
 	$(TARGET_MAKE_ENV) $(MAKE1) $(QORIQ_PTP4L_MAKE_OPTS) -C $(@D) DESTDIR=$(TARGET_DIR) install; \
 	$(INSTALL) -d $(TARGET_DIR)/etc/ptp4l_cfg; \
 	$(INSTALL) $(@D)/configs/* $(TARGET_DIR)/etc/ptp4l_cfg

commit 57a77f76572c02cfd711e6ace57191cff130296e

package/nxp/qoriq-ptp4l: depend on Linux kernel
As explained in commit 24f79f6ddb05 ("package/nxp/qoriq-ptp4l: use
compiled kernel headers"), linuxptp needs to use the compiled kernel
headers, and for that, the kernel needs to be compiled first. So,
linuxptp should have the kernel as a dependency.

Not sure how come this dependency got missed.

Fixes: 24f79f6ddb05 ("package/nxp/qoriq-ptp4l: use compiled kernel headers")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

package/nxp/qoriq-ptp4l/qoriq-ptp4l.mk
@@ -9,6 +9,7 @@ QORIQ_PTP4L_SITE = $(call qstrip,$(BR2_PACKAGE_QORIQ_PTP4L_SITE))
 QORIQ_PTP4L_SITE_METHOD = git
 QORIQ_PTP4L_LICENSE = GPL2
 QORIQ_PTP4L_LICENSE_FILES = COPYING
+QORIQ_PTP4L_DEPENDENCIES = linux
 
 QORIQ_PTP4L_MAKE_OPTS = \
 	prefix=/usr \