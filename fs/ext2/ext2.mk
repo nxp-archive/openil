559545a41a8bd781910c00a28acf10b5bf3fc5ed
Wenbin Song <wenbin.song@nxp.com>
2020-08-04 16:18:46 +0800

    rootfs/ext2: make sure the free blocks is enough to system start
    Create a new ext2 disk with the free blocks is equal to the 10 percent
    of the disk space. And the free blocks must be greater than 50MB at
    least.
    
    Signed-off-by: Wenbin Song <wenbin.song@nxp.com>

--- fs/ext2/ext2.mk
+++ fs/ext2/ext2.mk
@@ -25,23 +25,42 @@ ROOTFS_EXT2_DEPENDENCIES = host-e2fsprogs
 
 define ROOTFS_EXT2_CMD
 	rm -f $@
-	if [ $(EXT2_SIZE)x != x ]
-        then
-	    echo "The size of rootfs.ext2 is $(EXT2_SIZE)"
-	    $(HOST_DIR)/sbin/mkfs.ext$(BR2_TARGET_ROOTFS_EXT2_GEN) $(EXT2_OPTS) $@ \
-            "$(EXT2_SIZE)" || { ret=$$?; \
-	     echo "*** Maybe you need to increase the filesystem size (BR2_TARGET_ROOTFS_EXT2_SIZE)" 1>&2; \
-	     exit $$ret; }
+	if [ "x$(EXT2_SIZE)" != "x" ]
+	then
+		echo "The size of rootfs.ext2 is $(EXT2_SIZE)"
+		$(HOST_DIR)/sbin/mkfs.ext$(BR2_TARGET_ROOTFS_EXT2_GEN) $(EXT2_OPTS) $@ \
+			"$(EXT2_SIZE)" || { ret=$$?; \
+		echo "*** Maybe you need to increase the filesystem size (BR2_TARGET_ROOTFS_EXT2_SIZE)" 1>&2; \
+		exit $$ret; }
 	else
-	    EXT2_SIZE_E=`du -s  $(TARGET_DIR) | cut -f 1`
-            while true
-	    do
-	        EXT2_SIZE_E=$$(($${EXT2_SIZE_E} + 32 * 1024))
-	        echo "The size of rootfs.ext2 will set to $${EXT2_SIZE_E}"
-	        $(HOST_DIR)/sbin/mkfs.ext$(BR2_TARGET_ROOTFS_EXT2_GEN) $(EXT2_OPTS) $@ "$${EXT2_SIZE_E}K" \
-	        && break
-	    done
+		EXT2_SIZE_E=`du -s  $(TARGET_DIR) | cut -f 1`
+		while true
+		do
+			EXT2_SIZE_E=$$(($${EXT2_SIZE_E} + 100 * 1024))
+			echo "The size of rootfs.ext2 will set to $${EXT2_SIZE_E}"
+			$(HOST_DIR)/sbin/mkfs.ext$(BR2_TARGET_ROOTFS_EXT2_GEN) $(EXT2_OPTS) $@ "$${EXT2_SIZE_E}K" \
+			&& break
+		done
+
+		FREE_BLOCK=`dumpe2fs $@  2>/dev/null  | grep -i "^Free blocks" | awk -F ':' '{print $$2}'`
+		BLOCK_COUNT=`dumpe2fs $@  2>/dev/null  | grep -i "^Block count" | awk -F ':' '{print $$2}'`
+		BLOCK_SIZE=`dumpe2fs $@  2>/dev/null  | grep -i "^Block size" | awk -F ':' '{print $$2}'`
+
+		FREE_BLOCK=$$(($${FREE_BLOCK}*$${BLOCK_SIZE}/1024))
+		BLOCK_COUNT=$$(($${BLOCK_COUNT}*$${BLOCK_SIZE}/1024))
+		USED_BLOCK=$$(($${BLOCK_COUNT}-$${FREE_BLOCK}))
+
+		EXT2_SIZE_E=$$(($${USED_BLOCK}*100/90))
+
+		if [ $$(($${EXT2_SIZE_E} - $${USED_BLOCK})) -le 51200 ]
+		then
+			EXT2_SIZE_E=$$(($${USED_BLOCK} + 51200))
+		fi
+
+		rm -f $@
+		$(HOST_DIR)/sbin/mkfs.ext$(BR2_TARGET_ROOTFS_EXT2_GEN) $(EXT2_OPTS) $@ "$${EXT2_SIZE_E}K"
 	fi
+
 endef
 
 ifneq ($(BR2_TARGET_ROOTFS_EXT2_GEN),2)
