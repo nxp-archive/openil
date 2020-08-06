7983625941f84812b13d463d861d35f24e0fd3e5
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2020-08-06 17:30:52 +0800

    package: ethtool: add preempt support
    Update ethtool package to latest version, and add patches to support
    frame preempt which is defined by IEEE 802.1Qbu.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,12 +4,14 @@
 #
 ################################################################################
 
-ETHTOOL_VERSION = 5.4
+ETHTOOL_VERSION = 5.7
 ETHTOOL_SOURCE = ethtool-$(ETHTOOL_VERSION).tar.xz
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
 ETHTOOL_LICENSE = GPL-2.0
 ETHTOOL_LICENSE_FILES = LICENSE COPYING
+ETHTOOL_DEPENDENCIES = libmnl
 ETHTOOL_CONF_OPTS = \
 	$(if $(BR2_PACKAGE_ETHTOOL_PRETTY_PRINT),--enable-pretty-dump,--disable-pretty-dump)
+ETHTOOL_CONF_OPTS += --enable-netlink
 
 $(eval $(autotools-package))
