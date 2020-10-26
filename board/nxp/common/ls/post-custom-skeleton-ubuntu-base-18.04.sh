nxp-archive/openil

commit 1c9edea444f062144d8524a3fcef5bf897510dca

post-custom-skeleton-ubuntu: rebuild iproute2
Because iproute2 is the basic package in ubuntu base,
which will replace the command OpenIL built tc command is modified.
Rebuild iproute2 to retrieve that one required.

Signed-off-by: Changming Huang <jerry.huang@nxp.com>

board/nxp/common/ls/post-custom-skeleton-ubuntu-base-18.04.sh
@@ -250,6 +250,11 @@ main()
 		sed -i "s/float(n\[0\])/float(n[0].split()[0])/" ${1}/usr/share/pyshared/lsb_release.py
 	fi
 
+	# rebuild iproute2 and use the tc command modified for target system
+	if grep -Eq "^BR2_PACKAGE_IPROUTE2=y$" ${BR2_CONFIG}; then
+		make iproute2-rebuild
+	fi
+
 	exit $?
 }
 

commit 6cf771147cdf52c330bdfa19e2f22975ff2c2b3c

post-custom-skeleton-ubuntu: preserve O variable when rebuilding iproute2
When we build an Ubuntu rootfs with the O (output location) variable
set, as follows:

make nxp_ls1021atsn_ubuntu_defconfig O=output-ls1021atsn-ubuntu
make O=output-ls1021atsn-ubuntu

the build will hang here, waiting for the user's prompt:

rm -f /opt/openil/output/build/iproute2-5.5.0/.stamp_staging_installed
rm -f /opt/openil/output/build/iproute2-5.5.0/.stamp_target_installed
rm -f /opt/openil/output/build/iproute2-5.5.0/.stamp_images_installed
rm -f /opt/openil/output/build/iproute2-5.5.0/.stamp_host_installed
rm -f /opt/openil/output/build/iproute2-5.5.0/.stamp_built
/usr/bin/make -j1 O=/opt/openil/output HOSTCC="/usr/bin/gcc" HOSTCXX="/usr/bin/g++" syncconfig
*
* Restart config...
*
*
* Target options
*
Target Architecture
  1. ARC (little endian) (BR2_arcle)
  2. ARC (big endian) (BR2_arceb)
  3. ARM (little endian) (BR2_arm)
  4. ARM (big endian) (BR2_armeb)
> 5. AArch64 (little endian) (BR2_aarch64)
  6. AArch64 (big endian) (BR2_aarch64_be)
  7. csky (BR2_csky) (NEW)
  8. i386 (BR2_i386)
  9. m68k (BR2_m68k)
  10. Microblaze AXI (little endian) (BR2_microblazeel)
  11. Microblaze non-AXI (big endian) (BR2_microblazebe)
  12. MIPS (big endian) (BR2_mips)
  13. MIPS (little endian) (BR2_mipsel)
  14. MIPS64 (big endian) (BR2_mips64)
  15. MIPS64 (little endian) (BR2_mips64el)
  16. nds32 (BR2_nds32) (NEW)
  17. Nios II (BR2_nios2)
  18. OpenRISC (BR2_or1k)
  19. PowerPC (BR2_powerpc)
  20. PowerPC64 (big endian) (BR2_powerpc64)
  21. PowerPC64 (little endian) (BR2_powerpc64le)
  22. RISCV (BR2_riscv) (NEW)
  23. SuperH (BR2_sh)
  24. SPARC (BR2_sparc)
  25. SPARC64 (BR2_sparc64)
  26. x86_64 (BR2_x86_64)
  27. Xtensa (BR2_xtensa)
choice[1-27?]:

This is because the plain "make" command expects a .config in the
Buildroot root directory, but there is none there, because when O is
used, the config is located in $O/.config. So Buildroot attempts to
restart the config and asks the user for input, which is not what we
want.

The solution is to propagate the O variable, if it is set, to the make
subcommand that rebuilds iproute2.

Fixes: 1c9edea444f0 ("post-custom-skeleton-ubuntu: rebuild iproute2")

board/nxp/common/ls/post-custom-skeleton-ubuntu-base-18.04.sh
@@ -252,7 +252,7 @@ main()
 
 	# rebuild iproute2 and use the tc command modified for target system
 	if grep -Eq "^BR2_PACKAGE_IPROUTE2=y$" ${BR2_CONFIG}; then
-		make iproute2-rebuild
+		make iproute2-rebuild ${O:+O=$O}
 	fi
 
 	exit $?