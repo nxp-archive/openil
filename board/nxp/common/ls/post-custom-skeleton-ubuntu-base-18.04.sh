f5140b5f3dc74d7fba631c1f211fbb12b1082257
Changming Huang <jerry.huang@nxp.com>
2020-10-27 09:56:21 +0800

    skeleton-custom: bump to the version ubuntu-18.04.5
    Signed-off-by: Changming Huang <jerry.huang@nxp.com>

--- board/nxp/common/ls/post-custom-skeleton-ubuntu-base-18.04.sh
+++ board/nxp/common/ls/post-custom-skeleton-ubuntu-base-18.04.sh
@@ -88,7 +88,7 @@ do_distrorfs_first_stage() {
 	DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C \
 	sudo chroot $RFSDIR dpkg --configure -a
     fi
-    echo OpenIL-Ubuntu,18.04.4 | sudo tee $RFSDIR/etc/.firststagedone 1>/dev/null
+    echo OpenIL-Ubuntu,18.04.5 | sudo tee $RFSDIR/etc/.firststagedone 1>/dev/null
 
     sudo chroot $RFSDIR ubuntu-package-installer $1 $4 $5 $3 $6
     sudo chown -R $USER $RFSDIR
